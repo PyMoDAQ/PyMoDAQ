2847d6c09e5fc0602d89d226bf295cd99ec2f577
Sébastien Weber <seba.weber@gmail.com>
2020-05-01 09:40:37 +0200

    rtd

--- pymodaq/__init__.py
+++ pymodaq/__init__.py
@@ -1,6 +1,8 @@
-from .daq_utils import daq_utils as utils
-logger = utils.set_logger(utils.get_module_name(__file__), True, True)
-
+try:
+    from .daq_utils import daq_utils as utils
+    logger = utils.set_logger(utils.get_module_name(__file__), True, True)
+except:
+    print("Couldn't create the local folder to store logs , presets...")
 
 
 
