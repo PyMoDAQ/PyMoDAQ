155b56ad16bac4077d15a95b3687484ceb3952ca
Sébastien Weber <seba.weber@gmail.com>
2020-11-21 22:53:05 +0100

    Create custom_parameter_tree.py
    for back compatibility and warning on deprecation

---
+++ pymodaq/daq_utils/custom_parameter_tree.py
@@ -0,0 +1,8 @@
+from .daq_utils import set_logger
+logger = set_logger('custom_paramater', add_to_console=True)
+logger.warning('Calling the custom_parameter_module will soon be deprecated!\n'
+               'Please use either the ioxml, utils or pymodaq_ptypes submodules from the '
+               'pymodaq.daq_utils.parameter module')
+from .parameter.ioxml import *
+from .parameter.pymodaq_ptypes import *
+from .parameter.utils import *
