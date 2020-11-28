be3e6abb03a365cd4e8be98dd04314a61253d28c
Sébastien Weber <seba.weber@gmail.com>
2020-11-28 21:25:31 +0100

    configured Travis and tox

--- src/pymodaq/__init__.py
+++ src/pymodaq/__init__.py
@@ -3,7 +3,10 @@ __version__ = '2.3.2'
 try:
     # in a try statement for compilation on readthedocs server but if this fail, you cannot use the code
     from .daq_utils.daq_utils import set_logger, load_config, copy_preset
-    logger = set_logger('pymodaq', add_handler=True, base_logger=True)
+    try:
+        logger = set_logger('pymodaq', add_handler=True, base_logger=True)
+    except Exception:
+        print("Couldn't create the local folder to store logs , presets...")
     config = load_config()  # to ckeck for config file existence, otherwise create one
     copy_preset()
     logger.info('')
@@ -17,8 +20,4 @@ try:
     logger.info('')
 
 except Exception as e:
-    try:
-        logger.exception(str(e))
-        print("Couldn't create the local folder to store logs , presets...")
-    except Exception as e:
-        print("Couldn't create the local folder to store logs , presets...")
+    logger.exception(str(e))
