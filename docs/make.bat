9fef23bb1437183c799adf084f06386d3603b564
Sébastien Weber <seba.weber@gmail.com>
2021-01-08 22:56:05 +0100

    fixed doc configuration for separated src folder (src layout)

--- docs/make.bat
+++ docs/make.bat
@@ -7,9 +7,9 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=.
+set SOURCEDIR=./src
 set BUILDDIR=_build
-set SPHINXPROJ=DAQ_Pro_Aquisition
+set SPHINXPROJ=PyMoDAQ
 
 if "%1" == "" goto help
 
