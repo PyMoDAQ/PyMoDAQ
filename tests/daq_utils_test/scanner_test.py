8bcf7a30496f5e0e6baccc63f98e54f53439cd29
pierrejannot <85948876+pierrejannot@users.noreply.github.com>
2021-06-30 16:33:37 +0200

    Test developpment for scanner

--- tests/daq_utils_test/scanner_test.py
+++ tests/daq_utils_test/scanner_test.py
@@ -5,18 +5,61 @@ import pymodaq.daq_utils
 from pymodaq.daq_utils import scanner
 
 
-class TestScans:
-
+class TestScanInfo:
     def test_ScanInfo(self):
         Nsteps = 10
-        axes_indexes = []
-        axes_unique = []
-        positions = []
-        scan_param = scanner.ScanInfo(Nsteps, positions, axes_indexes, axes_unique)
+        axes_indexes = np.array([])
+        axes_unique = np.array([])
+        positions = np.array([])
+        kwargs = ['test1', 'test2']
+        scan_param = scanner.ScanInfo(Nsteps, positions, axes_indexes, axes_unique, kwargs=kwargs)
         assert scan_param.Nsteps is Nsteps
         assert scan_param.axes_indexes is axes_indexes
         assert scan_param.axes_unique is axes_unique
         assert scan_param.positions is positions
+        assert scan_param.kwargs is kwargs
+
+    def test__repr__(self):
+        Nsteps = 10
+        axes_indexes = np.array([])
+        axes_unique = np.array([])
+        positions = np.array([])
+        scan_param = scanner.ScanInfo(Nsteps, positions, axes_indexes, axes_unique)
+        assert scan_param.__repr__()
+
+
+class TestScanParameters:
+    def test_ScanParameters(self):
+        starts = [1, 2]
+        stops = [10, 20]
+        steps = [1, 2]
+        scan_param = scanner.ScanParameters(starts=starts, stops=stops, steps=steps)
+        assert scan_param.Naxes == 1
+        assert scan_param.scan_type == 'Scan1D'
+        assert scan_param.scan_subtype == 'Linear'
+        assert scan_param.starts == starts
+        assert scan_param.stops == stops
+        assert scan_param.steps == steps
+
+        with pytest.raises(ValueError):
+            scanner.ScanParameters(scan_type='test', starts=starts, stops=stops, steps=steps)
+
+        with pytest.raises(ValueError):
+            scanner.ScanParameters(scan_subtype='test', starts=starts, stops=stops, steps=steps)
+
+    def test_getattr(self):
+        starts = [1, 2]
+        stops = [10, 20]
+        steps = [1, 2]
+        positions = np.array([[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]])
+        axes_indexes = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]])
+        axes_unique = [np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])]
+        scan_param = scanner.ScanParameters(starts=starts, stops=stops, steps=steps)
+        assert scan_param.__getattr__('Nsteps') == 10
+        assert np.array_equal(scan_param.__getattr__('positions'), positions)
+        assert np.array_equal(scan_param.__getattr__('axes_indexes'), axes_indexes)
+        assert np.array_equal(scan_param.__getattr__('axes_unique'), axes_unique)
+        assert scan_param.__getattr__('adaptive_loss') == None
 
     def test_set_scan_spiral(self):
         nsteps = 10
