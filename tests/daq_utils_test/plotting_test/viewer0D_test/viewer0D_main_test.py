b01194b15e9dca35699d6efc045164c25125d405
Pierre Jannot <pierre.jannot@edu.esiee.fr>
2021-07-13 09:48:12 +0200

    viewer_0D main test

--- tests/daq_utils_test/plotting_test/viewer0D_test/viewer0D_main_test.py
+++ tests/daq_utils_test/plotting_test/viewer0D_test/viewer0D_main_test.py
@@ -7,19 +7,18 @@ import pytest
 from pymodaq.daq_utils.plotting.viewer0D import viewer0D_main as v_0D
 
 def test_0D_viewer(qtbot):
-    # Form = QtWidgets.QWidget()
-    # prog = v_0D.Viewer0D(Form)
-    # from pymodaq.daq_utils.daq_utils import gauss1D
-    #
-    # x = np.linspace(0, 200, 201)
-    # y1 = gauss1D(x, 75, 25)
-    # y2 = gauss1D(x, 120, 50, 2)
-    # Form.show()
-    # for ind, data in enumerate(y1):
-    #     prog.show_data([[data], [y2[ind]]])
-    #     QtWidgets.QApplication.processEvents()
-    #
-    # qtbot.addWidget(prog)
-    # qtbot.mouseClick(prog.ui.clear_pb, QtCore.Qt.LeftButton)
-    # assert np.array_equal(prog.x_axis, np.array([]))
-    pass
+    Form = QtWidgets.QWidget()
+    prog = v_0D.Viewer0D(Form)
+    from pymodaq.daq_utils.daq_utils import gauss1D
+
+    x = np.linspace(0, 200, 201)
+    y1 = gauss1D(x, 75, 25)
+    y2 = gauss1D(x, 120, 50, 2)
+    Form.show()
+    for ind, data in enumerate(y1):
+        prog.show_data([[data], [y2[ind]]])
+        QtWidgets.QApplication.processEvents()
+
+    qtbot.addWidget(prog)
+    qtbot.mouseClick(prog.ui.clear_pb, QtCore.Qt.LeftButton)
+    assert np.array_equal(prog.x_axis, np.array([]))
