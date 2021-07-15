dbd8bd9ae6a6cee0fa112b4560c27e2fb198fe66
Pierre Jannot <pierre.jannot@edu.esiee.fr>
2021-07-15 11:17:08 +0200

    Adding ExpectedError class in exceptions.py for pymodaq tests

--- src/pymodaq/daq_utils/exceptions.py
+++ src/pymodaq/daq_utils/exceptions.py
@@ -4,3 +4,19 @@ class DAQ_ScanException(Exception):
 
 class ScannerException(Exception):
     """Raised when there is an error related to the Scanner class (see pymodaq.da_utils.scanner)"""
+
+
+class ExpectedError(Exception):
+    """Raised in the tests made for pymodaq plugins"""
+
+
+class Expected_1(ExpectedError):
+    """Expected error 1 for pymodaq tests"""
+
+
+class Expected_2(ExpectedError):
+    """Expected error 2 for pymodaq tests"""
+
+
+class Expected_3(ExpectedError):
+    """Expected error 3 for pymodaq tests"""
