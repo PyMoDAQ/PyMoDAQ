c9334c2ff26302d4e091d869b2eeeec174aaa74b
Sébastien Weber <seba.weber@gmail.com>
2021-10-28 16:41:31 +0200

    introducing qtbackend selection using the qtpy package
    let the user choose which backend to use and make the usage of either PyQt5 or PySide2 transparent to PyMoDAQ

--- src/pymodaq/__init__.py
+++ src/pymodaq/__init__.py
@@ -1,4 +1,7 @@
 import importlib.util
+import os
+import sys
+
 from pathlib import Path
 
 try:
@@ -6,7 +9,7 @@ try:
         __version__ = fvers.read().strip()
 
     # in a try statement for compilation on readthedocs server but if this fail, you cannot use the code
-    from .daq_utils.daq_utils import set_logger, load_config, copy_preset
+    from .daq_utils.daq_utils import set_logger, load_config, copy_preset, setLocale, set_qt_backend
 
     try:
         logger = set_logger('pymodaq', add_handler=True, base_logger=True)
@@ -23,17 +26,27 @@ try:
             infos = "You have installed plugins requiring the pywin32 package to work correctly," \
                     " please type in *pip install pywin32* and restart PyMoDAQ"
             print(infos)
-        logger.warning(infos)
+            logger.warning(infos)
 
     config = load_config()  # to ckeck for config file existence, otherwise create one
     copy_preset()
-    logger.info('')
     logger.info('')
     logger.info('')
     logger.info('************************')
     logger.info('Starting PyMoDAQ modules')
     logger.info('************************')
     logger.info('')
+    logger.info('')
+    logger.info('************************')
+    logger.info(f"Setting Qt backend to: {config['qtbackend']['backend']} ...")
+    logger.info('************************')
+    set_qt_backend()
+    logger.info('')
+    logger.info('')
+    logger.info('************************')
+    logger.info(f"Setting Locale to {config['style']['language']} / {config['style']['country']}")
+    logger.info('************************')
+    setLocale()
     logger.info('')
     logger.info('')
 
