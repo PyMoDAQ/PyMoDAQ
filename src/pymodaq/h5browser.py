ae5f75dbc9387cdfeb67e94279d255e5a175fb98
rgeneaux <rgeneaux@gmail.com>
2022-04-28 10:39:38 +0200

    Add input-file option to h5browser.py to open chosen file directly.

--- src/pymodaq/h5browser.py
+++ src/pymodaq/h5browser.py
@@ -2,6 +2,8 @@ import sys
 from qtpy import QtWidgets
 from pymodaq.daq_utils.h5modules import H5Browser
 from pymodaq.daq_utils.config import Config
+import getopt, sys
+from pathlib import Path
 
 config = Config()
 
@@ -11,12 +13,22 @@ def main():
         import qdarkstyle
         app.setStyleSheet(qdarkstyle.load_stylesheet())
 
+    h5file_path = None
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "i:", ["input-file="])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+
+    for o, a in opts:
+        if o in ("-i", "--input-file"):
+            h5file_path = Path(a).resolve()  # Transform to absolute Path in case it is relative
+
     win = QtWidgets.QMainWindow()
-    prog = H5Browser(win)
+    prog = H5Browser(win, h5file_path=h5file_path)
     win.show()
     QtWidgets.QApplication.processEvents()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
