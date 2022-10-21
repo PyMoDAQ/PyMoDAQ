a91444c37b834900278ec3d9aa0c038c807f6503
Sébastien Weber <seba.weber@gmail.com>
2022-10-21 21:53:38 +0200

    Update move_utility_classes.py

--- src/pymodaq/control_modules/move_utility_classes.py
+++ src/pymodaq/control_modules/move_utility_classes.py
@@ -4,4 +4,4 @@ Created the 21/10/2022
 
 @author: Sebastien Weber
 """
-from pymodaq.daq_move.utility_classes import DAQ_Move_base, comon_parameters
+from pymodaq.daq_move.utility_classes import DAQ_Move_base, comon_parameters, comon_parameters_fun, main
