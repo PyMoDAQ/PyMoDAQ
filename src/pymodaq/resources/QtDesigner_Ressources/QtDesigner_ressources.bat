4b6c0292b6e679d50a5397bca56061130f14b10d
Sébastien Weber <seba.weber@gmail.com>
2022-10-25 15:20:12 +0200

    added run_all and stop_all icons

--- src/pymodaq/resources/QtDesigner_Ressources/QtDesigner_ressources.bat
+++ src/pymodaq/resources/QtDesigner_Ressources/QtDesigner_ressources.bat
@@ -1,2 +1,2 @@
 cd "C:\Users\Weber\Labo\Programmes Python\PyMoDAQ_Git\pymodaq\src\pymodaq\resources\QtDesigner_Ressources"
-C:\Miniconda3\envs\pyqt5tools\Scripts\pyrcc5 QtDesigner_ressources.qrc > QtDesigner_ressources_rc.py
+C:\Miniconda3\envs\qt5tools\Scripts\pyrcc5 QtDesigner_ressources.qrc > QtDesigner_ressources_rc.py
