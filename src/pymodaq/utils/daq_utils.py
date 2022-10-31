3cb921163acedb11e9ff5828e0b33c4b90e19153
Sébastien Weber <seba.weber@gmail.com>
2022-10-31 17:56:21 +0100

    added method to find objects in a list from a match attribute name/value

--- src/pymodaq/utils/daq_utils.py
+++ src/pymodaq/utils/daq_utils.py
@@ -451,6 +451,64 @@ def elt_as_first_element_dicts(elt_list, match_word='Mock', key='name'):
     return plugins
 
 
+def find_object_if_matched_attr_name_val(obj, attr_name, attr_value):
+    """check if an attribute  key/value pair match in a given object
+
+    Parameters
+    ----------
+    obj: object
+        list of objects
+    attr_name: str
+        attribute name to look for in the object
+    attr_value: object
+        value to match
+
+    Returns
+    -------
+    bool: True if the key/value pair has been found in dict_tmp
+
+    """
+    if hasattr(obj, attr_name):
+        if getattr(obj, attr_name) == attr_value:
+            return True
+    return False
+
+
+def find_objects_in_list_from_attr_name_val(objects, attr_name, attr_value, return_first=True):
+    """ lookup within a list of objects. Look for the objects within the list which has the correct attribute name,
+    value pair
+
+    Parameters
+    ----------
+    objects: list
+        list of objects
+    attr_name: str
+        attribute name to look for in the object
+    attr_value: object
+        value to match
+    return_first: bool
+        if True return the first objects found in the list else all the objects matching
+
+    Returns
+    -------
+    list of tuple(object, int): object and index or list of object and indexes
+    """
+    selection = []
+    obj = None
+    for ind, obj in enumerate(objects):
+        if find_object_if_matched_attr_name_val(obj, attr_name, attr_value):
+            if return_first:
+                break
+            else:
+                selection.append((obj, ind))
+
+    if obj is None:
+        if return_first:
+            return None, -1
+        else:
+            return selection
+
+
 def find_dict_if_matched_key_val(dict_tmp, key, value):
     """
     check if a key/value pair match in a given dictionnary
@@ -469,6 +527,8 @@ def find_dict_if_matched_key_val(dict_tmp, key, value):
         if dict_tmp[key] == value:
             return True
     return False
+
+
 
 
 def find_dict_in_list_from_key_val(dicts, key, value, return_index=False):
@@ -741,12 +801,12 @@ if __name__ == '__main__':
     # import license
     # mit = license.find('MIT')
     #
-    paths = recursive_find_expr_in_files('C:\\Users\\weber\\Labo\\Programmes Python\\PyMoDAQ_Git\\pymodaq\\docs',
-                                         exp="pymodaq.daq_utils",
+    paths = recursive_find_expr_in_files('C:\\Users\\weber\\Labo\\Programmes Python\\PyMoDAQ_Git\\pymodaq',
+                                         exp="dispatch",
                                          paths=[],
                                          filters=['.git', '.idea', '__pycache__', 'build', 'egg', 'documentation',
                                                   '.tox', 'daq_utils.py',],
-                                         replace=True,
+                                         replace=False,
                                          replace_str="pymodaq.utils")
     get_version()
     pass
