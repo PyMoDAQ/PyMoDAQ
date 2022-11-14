f898dabed7325416df4c8c4c65c2a2e21120143d
Sébastien Weber <seba.weber@gmail.com>
2022-11-14 17:33:40 +0100

    Introducing the ViewerFactory and the ViewerDispatcher
    to adapt automatically the viewer to the data type
    
    but use case may be troublesome as one need to reconnect signals when the viewer is changed, so maybe the connection mechanism should be thought again.
    
    Tried to apply it for the viewers/navigators of ViewerND, but all the connections and ROIs make the asynchrone machinery troublesome...reverted to manual stuff

--- src/pymodaq/utils/plotting/data_viewers/viewer.py
+++ src/pymodaq/utils/plotting/data_viewers/viewer.py
@@ -1,17 +1,151 @@
-from collections import OrderedDict
+
+
 from qtpy import QtWidgets
 from qtpy.QtCore import QObject, Signal, Slot
 
 from pymodaq.utils.data import DataToExport, DataRaw
-from pymodaq.utils.plotting.utils.filter import Filter
-from pymodaq.utils import daq_utils as utils
-from pymodaq.utils import gui_utils as gutils
 from pymodaq.utils.exceptions import ViewerError
-import datetime
+from pymodaq.utils.enums import BaseEnum
+from pymodaq.utils.factory import ObjectFactory, BuilderBase
+from pymodaq.utils.plotting import data_viewers
+from pymodaq.utils.managers.parameter_manager import ParameterManager
+
+config_viewers = {
+}
 
 
-DATATYPES = {'Viewer0D': 'Data0D', 'Viewer1D': 'Data1D', 'Viewer2D': 'Data2D', 'ViewerND': 'DataND',
-             'ViewerSequential': 'DataSequential'}
+class ViewersEnum(BaseEnum):
+    Viewer0D = 'Data0D'
+    Viewer1D = 'Data1D'
+    Viewer2D = 'Data2D'
+    ViewerND = 'DataND'
+    ViewerSequential = 'DataSequential'
+
+
+def get_viewer_enum_from_axes(Naxes: int):
+    if Naxes < 0:
+        raise ValueError('Naxes could not be below 0')
+    if Naxes == 0:
+        viewer_enum = ViewersEnum['Viewer0D']
+    elif Naxes == 1:
+        viewer_enum = ViewersEnum['Viewer1D']
+    elif Naxes == 2:
+        viewer_enum = ViewersEnum['Viewer2D']
+    else:
+        viewer_enum = ViewersEnum['ViewerND']
+    return viewer_enum
+
+
+class ViewerFactory(ObjectFactory):
+    def get(self, viewer_name, **kwargs):
+        if viewer_name not in ViewersEnum.names():
+            raise ValueError(f'{viewer_name} is not a valid PyMoDAQ Viewer: {ViewersEnum.names()}')
+        return self.create(viewer_name, **kwargs)
+
+    @property
+    def viewers(self):
+        return self.keys
+
+
+@ViewerFactory.register('Viewer0D')
+def create_viewer0D(parent: QtWidgets.QWidget = None, **_ignored):
+    return data_viewers.viewer0D.Viewer0D(parent)
+
+
+@ViewerFactory.register('Viewer1D')
+def create_viewer1D(parent: QtWidgets.QWidget, **_ignored):
+    return data_viewers.viewer1D.Viewer1D(parent)
+
+
+@ViewerFactory.register('Viewer2D')
+def create_viewer2D(parent: QtWidgets.QWidget, **_ignored):
+    return data_viewers.viewer2D.Viewer2D(parent)
+
+
+@ViewerFactory.register('ViewerND')
+def create_viewerND(parent: QtWidgets.QWidget, **_ignored):
+    return data_viewers.viewerND.ViewerND(parent)
+
+
+# @ViewerFactory.register('ViewerSequential')
+# def create_viewer_sequential(widget: QtWidgets.QWidget, **_ignored):
+#     return data_viewers.viewer_sequential.ViewerSequential(widget)
+
+
+viewer_factory = ViewerFactory()
+
+
+class ViewerDispatcher(QObject):
+
+    data_to_export_signal = Signal(DataToExport)
+    viewer_changed = Signal(object)
+
+    def __init__(self, parent_widget: QtWidgets.QWidget):
+        super().__init__()
+        self._parent: QtWidgets.QWidget = parent_widget
+
+        self._parent.setLayout(QtWidgets.QHBoxLayout())
+        self._parent.layout().setContentsMargins(0, 0, 0, 0)
+
+        self._viewer: ViewerBase = None
+        self._data: DataRaw = None
+
+        self.create_viewer('Viewer2D')
+
+    def __call__(self, *args, **kwargs):
+        return self._viewer
+
+    def show(self, show=True):
+        self._parent.setVisible(show)
+
+    def create_viewer(self, viewer_type: str):
+        self._clearall()
+        widget = QtWidgets.QWidget()
+        self._parent.layout().addWidget(widget)
+        self._viewer: ViewerBase = viewer_factory.get(viewer_type, parent=widget)
+        self._viewer.data_to_export_signal.connect(self.data_to_export_signal.emit)
+        self.viewer_changed.emit(self._viewer)
+        QtWidgets.QApplication.processEvents()
+        self._parent.show()
+
+    def _clearall(self):
+        children = []
+        for i in range(self._parent.layout().count()):
+            child = self._parent.layout().itemAt(i).widget()
+            if child:
+                children.append(child)
+                print(child)
+        for child in children:
+            child.deleteLater()
+
+    def show_data(self, data: DataRaw, **kwargs):
+        if self._viewer is None or self._data is None or data.dim != self._data.dim:
+            self.create_viewer(ViewersEnum(data.dim.name).name)
+
+        self._data = data
+        self._viewer.show_data(data, **kwargs)
+
+
+class ViewerDispatcherTest(ViewerDispatcher, ParameterManager):
+
+    params = [{'title': 'Show Viewer0D', 'name': 'viewer0D', 'type': 'action', 'visible': True},
+              {'title': 'Show Viewer1D', 'name': 'viewer1D', 'type': 'action', 'visible': True},
+              {'title': 'Show Viewer2D', 'name': 'viewer2D', 'type': 'action', 'visible': True},
+              {'title': 'Show ViewerND', 'name': 'viewerND', 'type': 'action', 'visible': True},
+              {'title': 'Show ViewerSequential', 'name': 'viewer_sequential', 'type': 'action', 'visible': True},]
+
+    def __init__(self, parent_widget: QtWidgets.QWidget):
+        super().__init__(parent_widget)
+
+        self.settings_tree.show()
+        self.connect_things()
+
+    def connect_things(self):
+        self.settings.child('viewer0D').sigActivated.connect(lambda: self.create_viewer('Viewer0D'))
+        self.settings.child('viewer1D').sigActivated.connect(lambda: self.create_viewer('Viewer1D'))
+        self.settings.child('viewer2D').sigActivated.connect(lambda: self.create_viewer('Viewer2D'))
+        self.settings.child('viewerND').sigActivated.connect(lambda: self.create_viewer('ViewerND'))
+        self.settings.child('viewer_sequential').sigActivated.connect(lambda: self.create_viewer('ViewerSequential'))
 
 
 class ViewerBase(QObject):
@@ -61,7 +195,7 @@ class ViewerBase(QObject):
     @property
     def viewer_type(self):
         """str: the viewer data type see DATA_TYPES"""
-        return DATATYPES[self.__class__.__name__]
+        return ViewersEnum[self.__class__.__name__].value
 
     def show_data(self, data: DataRaw, **kwargs):
         """Entrypoint to display data into the viewer
@@ -114,3 +248,13 @@ class ViewerBase(QObject):
     def setVisible(self, show=True):
         """convenience method to show or hide the paretn widget"""
         self.parent.setVisible(show)
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    widget = QtWidgets.QWidget()
+
+    prog = ViewerDispatcherTest(widget)
+
+    sys.exit(app.exec_())
