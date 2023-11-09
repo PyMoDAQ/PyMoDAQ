9780a3715c0f2d0e3cd6cf9e3a82e32a334c13b7
Sébastien Weber <seba.weber@gmail.com>
2023-11-09 10:17:12 +0100

    Update move_utility_classes.py
    make sure either axes_names or axis_names could be used

--- src/pymodaq/control_modules/move_utility_classes.py
+++ src/pymodaq/control_modules/move_utility_classes.py
@@ -90,8 +90,8 @@ def comon_parameters_fun(is_multiaxes=False, axes_names=[], axis_names=[], maste
     master: bool
         If True consider this plugin has to init the controller, otherwise use an already initialized instance
     """
-    if axis_names == [] and axes_names != []:
-        axis_names == axes_names
+    if axis_names == [] and len(axes_names) != 0:
+        axis_names = axes_names
 
     params = [
                  {'title': 'MultiAxes:', 'name': 'multiaxes', 'type': 'group', 'visible': is_multiaxes, 'children': [
