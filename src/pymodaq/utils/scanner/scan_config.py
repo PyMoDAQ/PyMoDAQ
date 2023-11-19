e8f278a1b5e2c323b0a718ef586f2605c3322614
Sébastien Weber <seba.weber@gmail.com>
2023-11-19 11:00:02 +0100

    create a config specific for scanners with default entries

---
+++ src/pymodaq/utils/scanner/scan_config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created the 19/11/2023
+
+@author: Sebastien Weber
+"""
+
+from pathlib import Path
+from pymodaq.utils.config import BaseConfig, getitem_recursive
+
+
+class ScanConfig(BaseConfig):
+    """Main class to deal with configuration values for this plugin"""
+    config_template_path = Path(__file__).parent.parent.parent.joinpath('resources/config_scan_template.toml')
+    config_name = f"config_{'scanner'}"
+
+    def __setitem__(self, key, value):
+        if isinstance(key, tuple):
+            dic = getitem_recursive(self._config, *key, ndepth=1, create_if_missing=True)
+            dic[key[-1]] = value
+        else:
+            self._config[key] = value
