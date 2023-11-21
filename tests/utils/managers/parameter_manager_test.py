ac49cda1a6c89a561f9e93b20ff1dfab827889f9
Sébastien Weber <seba.weber@gmail.com>
2023-11-21 16:59:20 +0100

    adding tests about saving/loading/updating settings

--- tests/utils/managers/parameter_manager_test.py
+++ tests/utils/managers/parameter_manager_test.py
@@ -4,12 +4,14 @@ Created the 07/11/2023
 
 @author: Sebastien Weber
 """
+from collections import OrderedDict
+import pytest
 from qtpy import QtWidgets
 
-import pytest
-import pytestqt
-
-
+from pymodaq.examples.parameter_ex import ParameterEx, Parameter
+from pymodaq.utils.parameter.utils import iter_children_params, compareParameters, compareStructureParameter,\
+    compareValuesParameter
+from pymodaq.utils.gui_utils.widgets.table import TableModel
 from pymodaq.utils.managers.parameter_manager import ParameterManager
 
 
@@ -46,4 +48,89 @@ def test_parameter_manager(qtbot):
     assert hasattr(param_manager.settings_tree, 'listAllItems')
 
 
+def test_save(qtbot, tmp_path):
+    ptree = ParameterEx()
+    ptree.settings_tree.show()
+    qtbot.addWidget(ptree.settings_tree)
 
+    file_path = tmp_path.joinpath('settings.xml')
+    ptree.save_settings(file_path)
+
+
+def test_load(qtbot, tmp_path):
+    ptree = ParameterEx()
+    ptree.settings_tree.show()
+    qtbot.addWidget(ptree.settings_tree)
+
+    file_path = tmp_path.joinpath('settings.xml')
+    ptree.save_settings(file_path)
+
+    parameter_copy = Parameter.create(name='settings', type='group', children=ParameterEx.params)
+    assert compareValuesParameter(ptree.settings, parameter_copy)
+
+    parameters = iter_children_params(ptree.settings, childlist=[])
+    parameters_copy = iter_children_params(parameter_copy, childlist=[])
+
+    for parameter in parameters:
+        if not parameter.hasChildren() and 'group' not in parameter.opts['type']:
+            default_parameter = Parameter.create(name='settings', type=parameter.opts['type'])
+            if not 'table' in parameter.opts['type']:
+                item = default_parameter.makeTreeItem(0)
+                if hasattr(item, 'widget'):
+                    parameter.setValue(item.widget.value())
+            elif 'tablewidget' == parameter.opts['type']:
+                parameter.setValue(OrderedDict(key1='data10', key2='25'))
+            elif 'tabular_table' == parameter.opts['type']:
+                parameter.setValue(TableModel([[0.5, 0.2, 0.6]], ['value20', 'val2', '555']))
+
+    assert not compareValuesParameter(ptree.settings, parameter_copy)
+    assert compareStructureParameter(ptree.settings, parameter_copy)
+
+    ptree.load_settings(file_path)
+    parameters = iter_children_params(ptree.settings, childlist=[])
+
+    for parameter, pcopy in zip(parameters, parameters_copy):
+        if parameter.value() != pcopy.value():
+            print(parameter)
+
+    assert compareValuesParameter(ptree.settings, parameter_copy)
+    assert compareStructureParameter(ptree.settings, parameter_copy)
+
+def test_load(qtbot, tmp_path):
+    ptree = ParameterEx()
+    ptree.settings_tree.show()
+    qtbot.addWidget(ptree.settings_tree)
+
+    file_path = tmp_path.joinpath('settings.xml')
+    ptree.save_settings(file_path)
+
+    parameter_copy = Parameter.create(name='settings', type='group', children=ParameterEx.params)
+    assert compareValuesParameter(ptree.settings, parameter_copy)
+
+    parameters = iter_children_params(ptree.settings, childlist=[])
+    parameters_copy = iter_children_params(parameter_copy, childlist=[])
+
+    for parameter in parameters:
+        if not parameter.hasChildren() and 'group' not in parameter.opts['type']:
+            default_parameter = Parameter.create(name='settings', type=parameter.opts['type'])
+            if not 'table' in parameter.opts['type']:
+                item = default_parameter.makeTreeItem(0)
+                if hasattr(item, 'widget'):
+                    parameter.setValue(item.widget.value())
+            elif 'tablewidget' == parameter.opts['type']:
+                parameter.setValue(OrderedDict(key1='data10', key2='25'))
+            elif 'tabular_table' == parameter.opts['type']:
+                parameter.setValue(TableModel([[0.5, 0.2, 0.6]], ['value20', 'val2', '555']))
+
+    assert not compareValuesParameter(ptree.settings, parameter_copy)
+    assert compareStructureParameter(ptree.settings, parameter_copy)
+
+    ptree.update_settings(file_path)
+    parameters = iter_children_params(ptree.settings, childlist=[])
+
+    for parameter, pcopy in zip(parameters, parameters_copy):
+        if parameter.value() != pcopy.value():
+            print(parameter)
+
+    assert compareValuesParameter(ptree.settings, parameter_copy)
+    assert compareStructureParameter(ptree.settings, parameter_copy)
