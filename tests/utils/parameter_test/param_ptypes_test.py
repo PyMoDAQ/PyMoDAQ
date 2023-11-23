26938f6477ec152f949798d0b1248cbd31802f58
Ashwolaa <schouderconstant@outlook.com>
2023-11-23 14:20:43 +0100

    new file for test with  ptypes

---
+++ tests/utils/parameter_test/param_ptypes_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created the 23/11/2023
+
+@author: Sebastien Weber
+"""
+
+import numpy as np
+import pytest
+
+from pymodaq.utils.parameter import Parameter,pymodaq_ptypes
+from pymodaq.utils.parameter import utils as putils
+
+
