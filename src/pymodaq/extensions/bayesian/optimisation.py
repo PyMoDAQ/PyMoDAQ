1311ba8f5529f959d5df09fdfaaf17b6901c99e7
Sébastien Weber <seba.weber@gmail.com>
2024-02-26 17:44:32 +0100

    first implementation of the bayesian optimisation extension

---
+++ src/pymodaq/extensions/bayesian/optimisation.py
@@ -0,0 +1,373 @@
+from typing import List, Union
+
+from pymodaq.utils import gui_utils as gutils
+from pymodaq.utils import daq_utils as utils
+from pymodaq.utils.parameter import utils as putils
+from qtpy import QtWidgets, QtCore
+import time
+import numpy as np
+from pymodaq.utils.data import DataToExport, DataToActuators, DataCalculated
+from pymodaq.utils.plotting.data_viewers.viewer0D import Viewer0D
+
+
+from pymodaq.utils.plotting.data_viewers.viewer import ViewerDispatcher
+from pymodaq.extensions.bayesian.utils import (get_bayesian_models, BayesianModelGeneric,
+                                               BayesianAlgorithm)
+
+from pymodaq.utils.gui_utils import QLED
+from pymodaq.utils.managers.modules_manager import ModulesManager
+
+
+logger = utils.set_logger(utils.get_module_name(__file__))
+
+EXTENSION_NAME = 'BayesianOptimisation'
+CLASS_NAME = 'BayesianOptimisation'
+
+
+class BayesianOptimisation(gutils.CustomApp):
+    command_runner = QtCore.Signal(utils.ThreadCommand)
+    models = get_bayesian_models()
+
+    params = [
+        {'title': 'Main Settings:', 'name': 'main_settings', 'expanded': True, 'type': 'group',
+         'children': [{'title': 'Ini. State', 'name': 'ini_random', 'type': 'int', 'value': 5},
+                      {'title': 'bounds', 'name': 'bounds', 'type': 'group', 'children': []}]},
+
+        {'title': 'Models', 'name': 'models', 'type': 'group', 'expanded': True, 'visible': True,
+         'children': [
+            {'title': 'Models class:', 'name': 'model_class', 'type': 'list',
+             'limits': [d['name'] for d in models]},
+            {'title': 'Model params:', 'name': 'model_params', 'type': 'group', 'children': []},
+        ]},
+        {'title': 'Move settings:', 'name': 'move_settings', 'expanded': True, 'type': 'group',
+         'visible': False, 'children': [
+             {'title': 'Units:', 'name': 'units', 'type': 'str', 'value': ''}]},
+
+    ]
+
+    def __init__(self, dockarea, dashboard):
+        super().__init__(dockarea, dashboard)
+
+        self.viewer_fitness: Viewer0D = None
+        self.viewer_observable: ViewerDispatcher = None
+        self.model_class: BayesianModelGeneric = None
+        self._modules_manager = ModulesManager(self.dashboard.detector_modules,
+                                               self.dashboard.actuators_modules)
+        self.modules_manager.settings.child('data_dimensions').setOpts(expanded=False)
+        self.modules_manager.settings.child('actuators_positions').setOpts(expanded=False)
+        self.setup_ui()
+
+    @property
+    def modules_manager(self) -> ModulesManager:
+        return self._modules_manager
+
+    def setup_docks(self):
+        """
+        to be subclassed to setup the docks layout
+        for instance:
+
+        self.docks['ADock'] = gutils.Dock('ADock name)
+        self.dockarea.addDock(self.docks['ADock"])
+        self.docks['AnotherDock'] = gutils.Dock('AnotherDock name)
+        self.dockarea.addDock(self.docks['AnotherDock"], 'bottom', self.docks['ADock"])
+
+        See Also
+        ########
+        pyqtgraph.dockarea.Dock
+        """
+        self.docks['settings'] = gutils.Dock('Settings')
+        self.dockarea.addDock(self.docks['settings'])
+        self.docks['settings'].addWidget(self.modules_manager.settings_tree)
+        self.docks['settings'].addWidget(self.settings_tree)
+
+        widget_observable = QtWidgets.QWidget()
+        widget_observable.setLayout(QtWidgets.QHBoxLayout())
+        observable_dockarea = gutils.DockArea()
+        widget_observable.layout().addWidget(observable_dockarea)
+        self.viewer_observable = ViewerDispatcher(observable_dockarea)
+        self.docks['observable'] = gutils.Dock('Observable')
+        self.dockarea.addDock(self.docks['observable'], 'right', self.docks['settings'])
+        self.docks['observable'].addWidget(widget_observable)
+
+        if len(self.models) != 0:
+            self.get_set_model_params(self.models[0]['name'])
+
+    def get_set_model_params(self, model_name):
+        self.settings.child('models', 'model_params').clearChildren()
+        if len(self.models) > 0:
+            model_class = utils.find_dict_in_list_from_key_val(self.models, 'name', model_name)['class']
+            main_params = getattr(model_class, 'main_params')
+            params = getattr(model_class, 'params')
+            self.settings.child('models', 'model_params').addChildren(main_params + params)
+
+    def setup_menu(self):
+        '''
+        to be subclassed
+        create menu for actions contained into the self.actions_manager, for instance:
+
+        For instance:
+
+        file_menu = self.menubar.addMenu('File')
+        self.actions_manager.affect_to('load', file_menu)
+        self.actions_manager.affect_to('save', file_menu)
+
+        file_menu.addSeparator()
+        self.actions_manager.affect_to('quit', file_menu)
+        '''
+        pass
+
+    def value_changed(self, param):
+        ''' to be subclassed for actions to perform when one of the param's value in self.settings is changed
+
+        For instance:
+        if param.name() == 'do_something':
+            if param.value():
+                print('Do something')
+                self.settings.child('main_settings', 'something_done').setValue(False)
+
+        Parameters
+        ----------
+        param: (Parameter) the parameter whose value just changed
+        '''
+        if param.name() == 'model_class':
+            self.get_set_model_params(param.value())
+        elif param.name() in putils.iter_children(self.settings.child('models', 'model_params'), []):
+            if self.model_class is not None:
+                self.model_class.update_settings(param)
+
+    def setup_actions(self):
+        logger.debug('setting actions')
+        self.add_action('quit', 'Quit', 'close2', "Quit program")
+        self.add_action('ini_model', 'Init Model', 'ini')
+        self.add_widget('model_led', QLED, toolbar=self.toolbar)
+        self.add_action('ini_runner', 'Init the Optimisation Algorithm', 'ini', checkable=True)
+        self.add_widget('runner_led', QLED, toolbar=self.toolbar)
+        self.add_action('run', 'Run Optimisation', 'run2', checkable=True)
+        self.add_action('pause', 'Pause Optimisation', 'pause', checkable=True)
+        logger.debug('actions set')
+
+    def connect_things(self):
+        logger.debug('connecting things')
+        self.connect_action('quit', self.quit, )
+        self.connect_action('ini_model', self.ini_model)
+        self.connect_action('ini_runner', self.ini_optimisation_runner)
+        self.connect_action('run', self.run_optimisation)
+        self.connect_action('pause', self.pause_runner)
+
+    def pause_runner(self):
+        self.command_runner.emit(utils.ThreadCommand('pause_PID', self.is_action_checked('pause')))
+
+    def quit(self):
+        self.dockarea.parent().close()
+
+    def set_model(self):
+        model_name = self.settings.child('models', 'model_class').value()
+        self.model_class = utils.find_dict_in_list_from_key_val(self.models, 'name', model_name)['class'](self)
+        self.model_class.ini_model()
+
+    def format_bounds(self, bounds: Parameter):
+        pass
+
+    def set_algorithm(self):
+        self.algorithm = BayesianAlgorithm(random_state=self.settings['main_settings', 'ini_random'],
+                                           bounds=self.format_bounds(self.settings['main_settings',
+                                           'bounds']),)
+
+    def ini_model(self):
+        try:
+            if self.model_class is None:
+                self.set_model()
+
+            self.modules_manager.selected_actuators_name = self.model_class.actuators_name
+            self.modules_manager.selected_detectors_name = self.model_class.detectors_name
+
+            self.enable_controls_opti(True)
+            self.get_action('model_led').set_as_true()
+            self.set_action_enabled('ini_model', False)
+
+            self.viewer_observable.update_viewers(['Viewer0D'] + self.model_class.observables_dim,
+                                                  ['Fitness', 'Individual'])
+
+        except Exception as e:
+            logger.exception(str(e))
+
+    def ini_optimisation_runner(self):
+        if self.is_action_checked('ini_runner'):
+            self.runner_thread = QtCore.QThread()
+            runner = OptimisationRunner(self.model_class, self.modules_manager)
+            self.runner_thread.runner = runner
+            runner.algo_output_signal.connect(self.process_output)
+            self.command_runner.connect(runner.queue_command)
+
+            runner.moveToThread(self.runner_thread)
+
+            self.runner_thread.start()
+            self.get_action('runner_led').set_as_true()
+
+        else:
+            if self.is_action_checked('run'):
+                self.get_action('run').trigger()
+                QtWidgets.QApplication.processEvents()
+
+            self.runner_thread.terminate()
+            self.get_action('runner_led').set_as_false()
+
+    def process_output(self, data: DataToExport):
+        self.viewer_observable.show_data(data)
+
+    def enable_controls_opti(self, enable: bool):
+        pass
+
+    def run_optimisation(self):
+        if self.is_action_checked('run'):
+            self.get_action('run').set_icon('stop')
+            self.command_runner.emit(utils.ThreadCommand('start', {}))
+            QtWidgets.QApplication.processEvents()
+            QtWidgets.QApplication.processEvents()
+            self.command_runner.emit(utils.ThreadCommand('run', {}))
+        else:
+            self.get_action('run').set_icon('run2')
+            self.command_runner.emit(utils.ThreadCommand('stop', {}))
+
+            QtWidgets.QApplication.processEvents()
+
+
+class OptimisationRunner(QtCore.QObject):
+    algo_output_signal = QtCore.Signal(DataToExport)
+
+    def __init__(self, model_class: BayesianModelGeneric, modules_manager: ModulesManager):
+        super().__init__()
+
+        self.det_done_datas: DataToExport = None
+        self.input_from_dets: float = None
+        self.outputs: List[np.ndarray] = []
+        self.dte_actuators: DataToExport = None
+
+        self.model_class: BayesianModelGeneric = model_class
+        self.modules_manager: ModulesManager = modules_manager
+
+        self.running = True
+        self.paused = False
+
+        self.optimisation_algorithm: BayesianAlgorithm = self.model_class.bayesian_algorithm
+
+    @QtCore.Slot(utils.ThreadCommand)
+    def queue_command(self, command: utils.ThreadCommand):
+        """
+        """
+        if command.command == "run":
+            self.run_opti(**command.attribute)
+
+        elif command.command == "pause":
+            self.pause_opti(command.attribute)
+
+        elif command.command == "stop":
+            self.running = False
+
+    def pause_opti(self, pause_state: bool):
+        self.paused = pause_state
+
+    def run_opti(self, sync_detectors=True, sync_acts=False):
+        """Start the optimisation loop
+
+        Parameters
+        ----------
+        sync_detectors: (bool) if True will make sure all selected detectors (if any) all got their data before calling
+            the model
+        sync_acts: (bool) if True will make sure all selected actuators (if any) all reached their target position
+         before calling the model
+        """
+        self.running = True
+        try:
+            if sync_detectors:
+                self.modules_manager.connect_detectors()
+            if sync_acts:
+                self.modules_manager.connect_actuators()
+
+            self.current_time = time.perf_counter()
+            logger.info('Optimisation loop starting')
+            while self.running:
+                if not self.paused:
+                    next_target = self.optimisation_algorithm.ask()
+
+                    self.outputs = next_target
+                    self.output_to_actuators: DataToActuators =\
+                        self.model_class.convert_output(self.outputs)
+
+                    self.modules_manager.move_actuators(self.output_to_actuators,
+                                                        self.output_to_actuators.mode,
+                                                        polling=False)
+
+                    # Do the evaluation (measurements)
+                    self.det_done_datas = self.modules_manager.grab_datas()
+                    self.input_from_dets = self.model_class.convert_input(self.det_done_datas)
+
+                    # Run the algo internal mechanic
+                    self.optimisation_algorithm.tell(float(self.input_from_dets.data[0].data[0][0]))
+
+                    dte = DataToExport('algo',
+                                       data=[self.individual_as_data(np.array([self.optimisation_algorithm.best_fitness]),
+                                                                     'Fitness'),
+                                             self.individual_as_data(self.optimisation_algorithm.best_individual,
+                                                                     'Individual'),
+                                             ])
+                    self.algo_output_signal.emit(dte)
+
+                self.current_time = time.perf_counter()
+                QtWidgets.QApplication.processEvents()
+
+
+            logger.info('Optimisation loop exiting')
+            self.modules_manager.connect_actuators(False)
+            self.modules_manager.connect_detectors(False)
+
+        except Exception as e:
+            logger.exception(str(e))
+
+    @staticmethod
+    def individual_as_data(individual: np.ndarray, name: str = 'Individual') -> DataCalculated:
+        return DataCalculated(name, data=[np.atleast_1d(np.squeeze(coordinate)) for coordinate in
+                                          np.atleast_1d(np.squeeze(individual))])
+
+
+def main(init_qt=True):
+    import sys
+    from pathlib import Path
+    from pymodaq.utils.daq_utils import get_set_preset_path
+
+    if init_qt:  # used for the test suite
+        app = QtWidgets.QApplication(sys.argv)
+
+        import qdarkstyle
+        app.setStyleSheet(qdarkstyle.load_stylesheet())
+
+    from pymodaq.dashboard import DashBoard
+
+    win = QtWidgets.QMainWindow()
+    area = gutils.dock.DockArea()
+    win.setCentralWidget(area)
+    win.resize(1000, 500)
+    win.setWindowTitle('PyMoDAQ Dashboard')
+
+    dashboard = DashBoard(area)
+    daq_scan = None
+    file = Path(get_set_preset_path()).joinpath(f"{'complex_data'}.xml")
+    if file.exists():
+        dashboard.set_preset_mode(file)
+        daq_scan = dashboard.load_extension_from_name('BayesianOptimisation')
+    else:
+        msgBox = QtWidgets.QMessageBox()
+        msgBox.setText(f"The default file specified in the configuration file does not exists!\n"
+                       f"{file}\n"
+                       f"Impossible to load the DAQScan Module")
+        msgBox.setStandardButtons(msgBox.Ok)
+        ret = msgBox.exec()
+
+    if init_qt:
+        sys.exit(app.exec_())
+    return dashboard, daq_scan, win
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ src/pymodaq/extensions/bayesian/utils.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+"""
+Created the 31/08/2023
+
+@author: Sebastien Weber
+"""
+from abc import ABC, abstractproperty, abstractmethod
+from typing import List
+from pathlib import Path
+import importlib
+import pkgutil
+import inspect
+import numpy as np
+
+
+from bayes_opt import BayesianOptimization
+from bayes_opt import UtilityFunction
+
+
+from pymodaq.extensions.pid.utils import DataToExport
+from pymodaq.utils.managers.modules_manager import ModulesManager
+from pymodaq.utils.daq_utils import find_dict_in_list_from_key_val, get_entrypoints
+from pymodaq.utils.logger import set_logger, get_module_name
+from pymodaq.utils.plotting.data_viewers.viewer import ViewersEnum
+from pymodaq.utils.parameter import Parameter
+from pymodaq.utils.data import DataToActuators
+
+
+logger = set_logger(get_module_name(__file__))
+
+
+class BayesianAlgorithm:
+
+    def __init__(self, ini_random: int, bounds: dict, **kwargs):
+
+        self._algo = BayesianOptimization(f=None,
+                                          random_state=ini_random,
+                                          pbounds=bounds,
+                                          **kwargs
+                                          )
+
+        self._utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0.0)
+
+    def ask(self) -> np.ndarray:
+        self._next_points = self._algo.space.params_to_array(self._algo.suggest(self._utility))
+        return self._next_points
+
+    def tell(self, function_value: float):
+        self._algo.register(params=self._next_points, target=function_value)
+
+    @property
+    def best_fitness(self) -> float:
+        return self._algo.max['target']
+
+    @property
+    def best_individual(self) -> np.ndarray:
+        return self._algo.space.params_to_array(self._algo.max['params'])
+
+
+class BayesianModelGeneric(ABC):
+
+    optimisation_algorithm: BayesianAlgorithm = BayesianAlgorithm
+
+    actuators_name: List[str] = []
+    detectors_name: List[str] = []
+
+    observables_dim: List[ViewersEnum] = []
+
+    params = []  # to be subclassed
+
+    def __init__(self, optimisation_controller: 'Optimisation'):
+        self.optimisation_controller = optimisation_controller  # instance of the pid_controller using this model
+        self.modules_manager: ModulesManager = optimisation_controller.modules_manager
+
+        self.settings = self.optimisation_controller.settings.child('models', 'model_params')  # set of parameters
+        self.check_modules(self.modules_manager)
+
+    def check_modules(self, modules_manager):
+        for act in self.actuators_name:
+            if act not in modules_manager.actuators_name:
+                logger.warning(f'The actuator {act} defined in the model is'
+                               f' not present in the Dashboard')
+                return False
+        for det in self.detectors_name:
+            if det not in modules_manager.detectors_name:
+                logger.warning(f'The detector {det} defined in the model is'
+                               f' not present in the Dashboard')
+
+    def update_detector_names(self):
+        names = self.optimisation_controller.settings.child(
+            'main_settings', 'detector_modules').value()['selected']
+        self.data_names = []
+        for name in names:
+            name = name.split('//')
+            self.data_names.append(name)
+
+    def update_settings(self, param: Parameter):
+        """
+        Get a parameter instance whose value has been modified by a user on the UI
+        To be overwritten in child class
+        """
+        ...
+
+    def ini_model(self):
+        self.modules_manager.selected_actuators_name = self.actuators_name
+        self.modules_manager.selected_detectors_name = self.detectors_name
+
+    def convert_input(self, measurements: DataToExport) -> float:
+        """
+        Convert the measurements in the units to be fed to the Optimisation Controller
+        Parameters
+        ----------
+        measurements: DataToExport
+            data object exported from the detectors from which the model extract a float value
+            (fitness) to be fed to the algorithm
+
+        Returns
+        -------
+        float
+
+        """
+        raise NotImplementedError
+
+    def convert_output(self, outputs: List[np.ndarray]) -> DataToActuators:
+        """ Convert the output of the Optimisation Controller in units to be fed into the actuators
+        Parameters
+        ----------
+        outputs: list of numpy ndarray
+            output value from the controller from which the model extract a value of the same units as the actuators
+
+        Returns
+        -------
+        DataToActuatorOpti: derived from DataToExport. Contains value to be fed to the actuators with a a mode
+            attribute, either 'rel' for relative or 'abs' for absolute.
+
+        """
+        raise NotImplementedError
+
+
+class BayesianModelDefault(BayesianModelGeneric):
+
+    actuators_name: List[str] = []  # to be populated dynamically at instantiation
+    detectors_name: List[str] = []  # to be populated dynamically at instantiation
+    observables_dim: List[ViewersEnum] = []
+
+    params =  [{'title': 'Optimizing signal', 'name': 'optimizing_signal', 'type': 'group',
+                'children': [
+                    {'title': 'Get data', 'name': 'data_probe', 'type': 'bool_push'},
+                    {'title': 'Optimize 0Ds:', 'name': 'optimize_0d', 'type': 'itemselect',
+                     'checkbox': True},
+        ]},]
+
+    def __init__(self, optimisation_controller: 'Optimisation'):
+        self.actuators_name = optimisation_controller.modules_manager.actuators_name
+        self.detectors_name = optimisation_controller.modules_manager.detectors_name
+        super().__init__(optimisation_controller)
+
+    def ini_model(self):
+        pass
+
+    def optimize_from(self):
+        self.modules_manager.get_det_data_list()
+        data0D = self.modules_manager.settings['data_dimensions', 'det_data_list0D']
+        data0D['selected'] = data0D['all_items']
+        self.settings.child('optimizing_signal', 'optimize_0d').setValue(data0D)
+
+    def update_settings(self, param: Parameter):
+        if param.name() == 'data_probe':
+            self.optimize_from()
+
+    def convert_input(self, measurements: DataToExport) -> float:
+        """ Convert the measurements in the units to be fed to the Optimisation Controller
+
+        Parameters
+        ----------
+        measurements: DataToExport
+            data object exported from the detectors from which the model extract a float value
+            (fitness) to be fed to the algorithm
+
+        Returns
+        -------
+        float
+
+        """
+        data_name: str = self.settings['optimizing_signal', 'optimize_0d']['selected'][0]
+        origin, name = data_name.split('//')
+        return measurements.get_data_from_name_origin(name, origin)
+
+
+def get_bayesian_models(model_name=None):
+    """
+    Get PID Models as a list to instantiate Control Actuators per degree of liberty in the model
+
+    Returns
+    -------
+    list: list of disct containting the name and python module of the found models
+    """
+    models_import = []
+    discovered_models = get_entrypoints(group='pymodaq.models')
+    if len(discovered_models) > 0:
+        for pkg in discovered_models:
+            try:
+                module = importlib.import_module(pkg.value)
+                module_name = pkg.value
+
+                for mod in pkgutil.iter_modules([str(Path(module.__file__).parent.joinpath('models'))]):
+                    try:
+                        model_module = importlib.import_module(f'{module_name}.models.{mod.name}', module)
+                        classes = inspect.getmembers(model_module, inspect.isclass)
+                        for name, klass in classes:
+                            if isinstance(klass.__name__, BayesianModelGeneric):
+                                models_import.append({'name': mod.name, 'module': model_module, 'class': klass})
+                                break
+
+                    except Exception as e:  # pragma: no cover
+                        logger.warning(str(e))
+
+            except Exception as e:  # pragma: no cover
+                logger.warning(f'Impossible to import the {pkg.value} extension: {str(e)}')
+
+    models_import.append({'name': 'BayesianModelDefault',
+                          'module': inspect.getmodule(BayesianModelDefault),
+                          'class': BayesianModelDefault})
+
+    if model_name is None:
+        return models_import
+    else:
+        return find_dict_in_list_from_key_val(models_import, 'name', model_name)
