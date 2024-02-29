412ad817d9c5750b48a091cebfb3a03d5ed05bd5
Sébastien Weber <seba.weber@gmail.com>
2024-02-29 17:50:02 +0100

    copy axis before increasing its index otherwise initial object is changed

--- src/pymodaq/utils/h5modules/data_saving.py
+++ src/pymodaq/utils/h5modules/data_saving.py
@@ -12,21 +12,14 @@ import numpy as np
 
 from pymodaq.utils.abstract import ABCMeta, abstract_attribute
 from pymodaq.utils.enums import enum_checker
-from pymodaq.utils.data import Axis, DataDim, DataWithAxes, DataToExport, DataDistribution, DataDimError
+from pymodaq.utils.data import (Axis, DataDim, DataWithAxes, DataToExport, DataDistribution,
+                                DataDimError, squeeze)
 from .saving import DataType, H5Saver
 from .backends import GROUP, CARRAY, Node, EARRAY, NodeError
 from pymodaq.utils.daq_utils import capitalize
 from pymodaq.utils.scanner.utils import ScanType
 
 SPECIAL_GROUP_NAMES = dict(nav_axes='NavAxes')
-
-
-def squeeze(data_array: np.ndarray, do_squeeze=True) -> np.ndarray:
-    """ Squeeze numpy arrays return at least 1D arrays except if do_squeeze is False"""
-    if do_squeeze:
-        return np.atleast_1d(np.squeeze(data_array))
-    else:
-        return np.atleast_1d(data_array)
 
 
 class AxisError(Exception):
@@ -520,6 +513,7 @@ class DataEnlargeableSaver(DataSaverLoader):
                                               enlargeable=True)
             if save_axes:
                 for axis in data.axes:
+                    axis = axis.copy()
                     axis.index += 1  # because of enlargeable data will have an extra shape
                     self._axis_saver.add_axis(where, axis)
 
@@ -548,11 +542,8 @@ class DataEnlargeableSaver(DataSaverLoader):
         if self.get_last_node_name(where) is None:
             if len(data.nav_indexes) == 0:
                 data_init = data
-            elif len(data.nav_indexes) == 1:
-                data_init = data.inav[0]
-                data_init.source = data.source  # because slicing returns a calculated one
             else:
-                raise DataDimError('It is not possible to append DataND with more than 1 navigation axis')
+                raise DataDimError('It is not possible to append DataND')
             self._create_data_arrays(where, data_init, save_axes=True, add_enl_axes=add_enl_axes)
 
         for ind_data in range(len(data)):
@@ -746,9 +737,9 @@ class DataToExportEnlargeableSaver(DataToExportSaver):
 
         super().__init__(h5saver)
         if enl_axis_names is None:  # for backcompatibility
-            enl_axis_names = [axis_name]
+            enl_axis_names = (axis_name,)
         if enl_axis_units is None:  # for backcompatibilitu
-            enl_axis_units = (axis_units)
+            enl_axis_units = (axis_units,)
 
         if len(enl_axis_names) != len(enl_axis_units):
             raise ValueError('Both enl_axis_names and enl_axis_units should have the same length')
