dce6dcc75889f9f935e00e25ed444ac0ae2c1e0a
Sébastien Weber <seba.weber@gmail.com>
2024-03-11 20:00:30 +0100

    Update parameter_manager_test.py

--- tests/utils/managers/parameter_manager_test.py
+++ tests/utils/managers/parameter_manager_test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+1# -*- coding: utf-8 -*-
 """
 Created the 07/11/2023
 
@@ -11,7 +11,7 @@ from qtpy import QtWidgets
 from pymodaq.examples.parameter_ex import ParameterEx, Parameter
 from pymodaq.utils.parameter.utils import (iter_children_params, compareParameters,
                                            compareStructureParameter,
-                                           compareValuesParameter, iter_get_values)
+                                           compareValuesParameter)
 from pymodaq.utils.gui_utils.widgets.table import TableModel
 from pymodaq.utils.managers.parameter_manager import ParameterManager
 
