f3b0d16d53343d3884fdc2e498b42c07feecb62c
Sébastien Weber <seba.weber@gmail.com>
2024-08-22 14:44:54 +0200

    add tests for DataFrom plugins and Actuator

--- tests/utils/data_test.py
+++ tests/utils/data_test.py
@@ -27,15 +27,39 @@ Nn0 = 10
 Nn1 = 5
 
 
+def init_axis(data=None, index=0) -> data_mod.Axis:
+    if data is None:
+        data = DATA
+    return data_mod.Axis(label=LABEL, units=UNITS, data=data, index=index)
+
+
+def init_data(data=None, Ndata=1, axes=[], name='myData', source=data_mod.DataSource['raw'],
+              labels=None, units='') -> data_mod.DataWithAxes:
+    if data is None:
+        data = DATA2D
+    return data_mod.DataWithAxes(name, units=units, source=source,
+                                 data=[data for ind in range(Ndata)],
+                                 axes=axes, labels=labels)
+
+
+class TestDataSource:
+    def test_data_from_plugins(self):
+        Ndata = 2
+        data = data_mod.DataFromPlugins('myData', data=[DATA2D for ind in range(Ndata)])
+        assert isinstance(data, data_mod.DataWithAxes)
+        assert data.source == data_mod.DataSource['raw']
+
+
 class TestDataFromPlugins:
     def test_attributes(self):
         dwa = data_mod.DataFromPlugins(name='blabla', data=[DATA1D])
 
         assert hasattr(dwa, 'do_plot')
-        assert dwa.do_plot == True
+        assert dwa.do_plot is True
 
         assert hasattr(dwa, 'do_save')
-        assert dwa.do_save == True
+        assert dwa.do_save is True
+
 
 
 class TestDataActuator:
@@ -60,3 +84,38 @@ class TestDataActuator:
         assert d.name == 'actuator'
         assert d.data[0] == np.array([data_number])
 
+    @pytest.mark.parametrize('datatmp', (DATA0D, DATA1D, DATA2D))
+    def test_comparison_data_actuator(self, datatmp):
+        LENGTH = 3
+        data = init_data(datatmp, LENGTH)
+        data_eq = init_data(datatmp, LENGTH)
+        data_lt = init_data(datatmp - 0.01 * np.ones(datatmp.shape), LENGTH)
+        data_gt = init_data(datatmp + 0.01 * np.ones(datatmp.shape), LENGTH)
+
+        assert data == data_eq
+        assert data >= data_eq
+        assert data <= data_eq
+        assert data > data_lt
+        assert data < data_gt
+
+    def test_comparison_numbers(self):
+        LENGTH = 1
+        data = init_data(DATA0D, LENGTH)
+        data_eq = float(DATA0D[0])
+        data_lt = float(DATA0D[0]) - 0.01
+        data_gt = float(DATA0D[0]) + 0.01
+
+        assert data == data_eq
+        assert data >= data_eq
+        assert data <= data_eq
+        assert data > data_lt
+        assert data < data_gt
+
+        ARRAY = np.array([1, 2, 1.5])
+        data = data_mod.DataActuator(data=[ARRAY])
+        assert not data > 1
+        assert data > 0.999
+        assert data >= 1
+        assert data == data_mod.DataActuator(data=[ARRAY])
+        assert data < 2.001
+        assert data <= 2
