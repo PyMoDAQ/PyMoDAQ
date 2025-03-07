dfe044631628147e513585071cf269a2ab1c3426
Sébastien Weber <seba.weber@gmail.com>
2025-03-07 15:03:35 +0100

    coorect import

--- src/pymodaq/extensions/h5browser.py
+++ src/pymodaq/extensions/h5browser.py
@@ -1,5 +1,8 @@
-from pymodaq_gui.h5modules.h5browser import main
+from pymodaq_gui.h5modules.h5browser import main, H5Browser  #backcompat
+from pymodaq_utils.utils import deprecation_msg
 
+
+deprecation_msg('H5Browser should now be loaded from the pymodaq_gui.h5modules.h5browser module')
 
 
 if __name__ == '__main__':
