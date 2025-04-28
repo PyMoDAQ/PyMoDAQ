6da869184e2284f7e63453996333c7f818cc8737
Sébastien Weber <seba.weber@gmail.com>
2025-04-28 18:02:33 +0200

    testing for the presence of mandatory entries in config file

--- tests/utils/config_test.py
+++ tests/utils/config_test.py
@@ -45,3 +45,46 @@ class TestGetSet:
         overshoot_path = config_mod_pymodaq.get_set_overshoot_path()
         assert Path(overshoot_path) == Path(local_path).joinpath('overshoot_configs')
         assert Path(overshoot_path).is_dir()
+
+
+def test_required_config_entries():
+    config = config_mod_pymodaq.Config()
+    assert 'presets' in config
+    assert 'default_preset_for_scan' in config('presets')
+    assert 'default_preset_for_logger' in config('presets')
+    assert 'default_preset_for_pid' in config('presets')
+
+    assert 'actuator' in config
+    assert 'ui' in config('actuator')
+    assert 'epsilon_default' in config('actuator')
+    assert 'polling_interval_ms' in config('actuator')
+    assert 'polling_timeout_s' in config('actuator')
+    assert 'refresh_timeout_ms' in config('actuator')
+    assert 'timeout' in config('actuator')
+    assert 'siprefix' in config('actuator')
+    assert 'siprefix_even_without_units' in config('actuator')
+    assert 'display_units' in config('actuator')
+
+    assert 'binary' in config('actuator')
+    assert 'value_1' in config('actuator', 'binary')
+    assert 'value_2' in config('actuator', 'binary')
+
+    assert 'viewer' in config
+    assert 'daq_type' in config('viewer')
+    assert 'viewer_in_thread' in config('viewer')
+    assert 'timeout' in config('viewer')
+    assert 'allow_settings_edition' in config('viewer')
+
+    assert 'scan' in config
+    assert 'scan_in_thread' in config('scan')
+    assert 'show_popups' in config('scan')
+    assert 'default' in config('scan')
+    assert 'Naverage' in config('scan')
+    assert 'average_on_top' in config('scan')
+    assert 'steps_limit' in config('scan')
+    assert 'sort1D' in config('scan')
+
+    assert 'timeflow' in config('scan')
+    assert 'wait_time' in config('scan', 'timeflow')
+    assert 'wait_time_between' in config('scan', 'timeflow')
+    assert 'timeout' in config('scan', 'timeflow')
