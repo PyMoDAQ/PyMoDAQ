8cf2d5699c8affdc67e029fe2873780802885cfb
Sébastien Weber <seba.weber@gmail.com>
2025-07-01 16:02:04 +0200

    handles various case depending the item in the list

--- src/pymodaq/control_modules/daq_move.py
+++ src/pymodaq/control_modules/daq_move.py
@@ -518,7 +518,12 @@ class DAQ_Move(ParameterControlModule):
         Mostly to make sure DAQ_Move is backcompatible with old style plugins
         """
         if isinstance(data_act, list):  # backcompatibility
-            data_act = DataActuator(data=[np.atleast_1d(val) for val in data_act], units=self.units)
+            if isinstance(data_act[0], Number):
+                data_act = DataActuator(data=[np.atleast_1d(val) for val in data_act], units=self.units)
+            elif isinstance(data_act[0], np.ndarray):
+                data_act = DataActuator(data=data_act, units=self.units)
+            else:
+                raise TypeError('Unknown data type')
         if isinstance(data_act, np.ndarray):  # backcompatibility
             data_act = DataActuator(data=[data_act], units=self.units)
         data_act.name = self.title  # for the DataActuator name to be the title of the DAQ_Move
