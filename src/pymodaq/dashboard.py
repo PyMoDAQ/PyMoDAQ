13df667436315ad8c227cd0f704d0039e89e9456
Arnaud Meyer <arnaud.meyer@univ-st-etienne.fr>
2025-07-04 14:42:48 +0200

    Retrieve dashboard startup functions in dashboard.py

--- src/pymodaq/dashboard.py
+++ src/pymodaq/dashboard.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from importlib import import_module
 from packaging import version as version_mod
 from typing import Tuple, List, Any, TYPE_CHECKING, Sequence
+import argparse
 
 
 from qtpy import QtGui, QtWidgets, QtCore
@@ -1802,3 +1803,37 @@ class DashBoard(CustomApp):
                 logging.info(txt)
         except Exception as e:
             pass
+
+
+def main():
+    from pymodaq_gui.utils.utils import mkQApp
+    from pymodaq.utils.gui_utils.loader_utils import load_dashboard_with_preset
+
+    # Create application and main window
+    app = mkQApp('Dashboard')
+    win = QtWidgets.QMainWindow()
+    area = DockArea()
+    win.setCentralWidget(area)
+    win.resize(1000, 500)
+    win.setWindowTitle('PyMoDAQ Dashboard')
+
+    # Command-line argument parsing
+    parser = argparse.ArgumentParser(prog="dashboard", description="PyMoDAQ dashboard")
+    parser.add_argument("-p", "--preset", metavar="PRESET_NAME", help="preset name to load")
+    args = parser.parse_args()
+
+    # If preset name is supplied, load dashboard with this preset
+    if args.preset:
+        load_dashboard_with_preset(args.preset)
+
+    # If no command-line arguments are supplied, start empty
+    else:
+        prog = DashBoard(area)
+        win.show()
+
+    # Run application
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
