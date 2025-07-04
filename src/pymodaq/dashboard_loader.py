eec5934f6cbf3cd4da2b3bfa15a729b11347dcac
Arnaud Meyer <arnaud.meyer@univ-st-etienne.fr>
2025-07-04 11:28:05 +0200

    Implemented -p (or --preset) argument to dashboard to load a preset by name

--- src/pymodaq/dashboard_loader.py
+++ src/pymodaq/dashboard_loader.py
@@ -5,6 +5,10 @@ from pymodaq.dashboard import DashBoard
 from pymodaq_gui.utils import DockArea
 from pymodaq_gui.utils.utils import mkQApp
 
+from pymodaq.utils.gui_utils.loader_utils import load_dashboard_with_preset
+
+import argparse
+
 
 def main():
 
@@ -16,9 +20,19 @@ def main():
     win.resize(1000, 500)
     win.setWindowTitle('PyMoDAQ Dashboard')
 
-    # Create and show dashboard
-    prog = DashBoard(area)
-    win.show()
+    # Command-line argument parsing
+    parser = argparse.ArgumentParser(prog="dashboard", description="PyMoDAQ dashboard")
+    parser.add_argument("-p", "--preset", metavar="PRESET_NAME", help="preset name to load")
+    args = parser.parse_args()
+
+    # If preset name is supplied, load dashboard with this preset
+    if args.preset:
+        load_dashboard_with_preset(args.preset)
+
+    # If no command-line arguments are supplied, start empty
+    else:
+        prog = DashBoard(area)
+        win.show()
 
     # Run application
     app.exec()
