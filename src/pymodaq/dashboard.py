e47cc91eb89f4a06b69392844d4a2ff68820f9fa
Ashwola <schouderconstant@outlook.com>
2025-07-06 18:19:04 +0200

    Removing class method in splash_sc for python >3.11

--- src/pymodaq/dashboard.py
+++ src/pymodaq/dashboard.py
@@ -259,12 +259,11 @@ class DashBoard(CustomApp):
             if self.check_update(show=False):
                 sys.exit(0)
 
-    @classmethod
     @property
-    def splash_sc(cls) -> QtWidgets.QSplashScreen:
-        if cls._splash_sc is None:
-            cls._splash_sc = get_splash_sc()
-        return cls._splash_sc
+    def splash_sc(self) -> QtWidgets.QSplashScreen:
+        if not hasattr(self, "_splash_sc") or self._splash_sc is None:
+            self._splash_sc = get_splash_sc()
+        return self._splash_sc
 
     def set_preset_path(self, path):
         self.preset_path = path
