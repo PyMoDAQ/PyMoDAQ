80e186c1448adc81ea5991aeb0f4767e5711b2aa
Benedikt Burger <67148916+BenediktBurger@users.noreply.github.com>
2025-07-18 16:28:16 +0200

    add method tests for listener

--- tests/utils/leco/test_pymodaq_listener.py
+++ tests/utils/leco/test_pymodaq_listener.py
@@ -4,6 +4,8 @@ from pyleco.json_utils.json_objects import ErrorResponse, ResultResponse, Reques
 from pyleco.json_utils.errors import RECEIVER_UNKNOWN, NODE_UNKNOWN
 from pyleco.test import FakeCommunicator
 import pytest
+
+from pymodaq.utils.leco.rpc_method_definitions import GenericMethods, MoveMethods, ViewerMethods
 from pymodaq.utils.leco.pymodaq_listener import ActorListener
 
 
@@ -15,6 +17,33 @@ def actorListener() -> ActorListener:
     listener.communicator = FakeCommunicator(name=name)  # type: ignore[assign]
     return listener
 
+
+discover_string = Request(1, "rpc.discover").model_dump_json()
+
+
+class Test_methods_presence:
+    @pytest.fixture(scope="class")
+    def methods(self) -> list[str]:
+        actorListener = ActorListener(name=name)
+        actorListener.start_listen()
+        response = actorListener.message_handler.rpc.process_request(discover_string)
+        assert response is not None
+        result = actorListener.message_handler.rpc_generator.get_result_from_response(response)
+        methods = result["methods"]
+        actorListener.stop_listen()
+        return [item["name"] for item in methods]
+
+    @pytest.mark.parametrize("method", GenericMethods)
+    def test_generic_methods_are_present(self, method, methods):
+        assert method in methods
+
+    @pytest.mark.parametrize("method", MoveMethods)
+    def test_move_methods_are_present(self, method, methods):
+        assert method in methods
+
+    @pytest.mark.parametrize("method", ViewerMethods)
+    def test_viewer_methods_are_present(self, method, methods):
+        assert method in methods
 
 
 class TestSendRPCToRemote:
