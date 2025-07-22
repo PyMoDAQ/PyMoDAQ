a21de17bee9ed0dc51134303aee4438381afdd8d
Christian Cabello <christian.cabello@ip-paris.fr>
2025-07-22 10:12:27 +0200

    Host arguments for daq directors

--- src/pymodaq/utils/leco/daq_xDviewer_LECODirector.py
+++ src/pymodaq/utils/leco/daq_xDviewer_LECODirector.py
@@ -32,8 +32,11 @@ class DAQ_xDViewer_LECODirector(LECODirector, DAQ_Viewer_base):
     socket_types = ["GRABBER"]
     params = comon_parameters + leco_parameters
 
-    def __init__(self, parent=None, params_state=None, grabber_type: str = "0D", **kwargs) -> None:
-        super().__init__(parent=parent, params_state=params_state, **kwargs)
+    def __init__(self, parent=None, params_state=None, grabber_type: str = "0D", host: str = None, **kwargs) -> None:
+        DAQ_Viewer_base.__init__(self, parent=parent, params_state=params_state)
+        if host is not None:
+            self.settings["host"] = host
+        LECODirector.__init__(self, host=self.settings["host"])
         self.register_rpc_methods((
             self.set_x_axis,
             self.set_y_axis,
