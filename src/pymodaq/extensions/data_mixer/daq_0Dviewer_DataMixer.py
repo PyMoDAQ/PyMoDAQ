505c38d5647def2b4591c7d40cf35f34df21e790
Sébastien Weber <seba.weber@gmail.com>
2025-09-25 22:56:12 +0200

    adding the mock DAQ_0DViewer_DataMixer instrument class into the core code

---
+++ src/pymodaq/extensions/data_mixer/daq_0Dviewer_DataMixer.py
@@ -0,0 +1,93 @@
+from typing import TYPE_CHECKING
+import numpy as np
+
+from pymodaq_utils.utils import ThreadCommand
+from pymodaq_data.data import DataToExport
+from pymodaq_gui.parameter import Parameter
+
+from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
+from pymodaq.utils.data import DataFromPlugins
+
+if TYPE_CHECKING:
+    from pymodaq.extensions.data_mixer import DataMixer
+
+
+class DAQ_0DViewer_DataMixer(DAQ_Viewer_base):
+    """ Instrument plugin class for a OD viewer.
+    
+    This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Viewer module through inheritance via
+    DAQ_Viewer_base. It makes a bridge between the DAQ_Viewer module and the Python wrapper of a particular instrument.
+
+    Attributes:
+    -----------
+    controller: object
+        The particular object that allow the communication with the hardware, in general a python wrapper around the
+         hardware library.
+
+    """
+    params = comon_parameters+[]
+
+    def ini_attributes(self):
+        self.controller: DataMixer = None
+
+    def commit_settings(self, param: Parameter):
+        """Apply the consequences of a change of value in the detector settings
+
+        Parameters
+        ----------
+        param: Parameter
+            A given parameter (within detector_settings) whose value has been changed by the user
+        """
+        pass
+
+    def ini_detector(self, controller=None):
+        """Detector communication initialization
+
+        Parameters
+        ----------
+        controller: (object)
+            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator/detector by controller
+            (Master case)
+
+        Returns
+        -------
+        info: str
+        initialized: bool
+            False if initialization failed otherwise True
+        """
+
+        self.controller: DataMixer = controller
+        if self.controller is not None:
+            self.controller.dte_computed_signal.connect(self.grab_done)
+
+        info = "DataMixer Detector Initialized"
+        initialized = True
+        return info, initialized
+
+    def close(self):
+        """Terminate the communication protocol"""
+        pass
+
+    def grab_done(self, dte: DataToExport):
+        self.dte_signal.emit(dte)
+
+    def grab_data(self, Naverage=1, **kwargs):
+        """Start a grab from the detector
+
+        Parameters
+        ----------
+        Naverage: int
+            Number of hardware averaging (if hardware averaging is possible, self.hardware_averaging should be set to
+            True in class preamble and you should code this implementation)
+        kwargs: dict
+            others optionals arguments
+        """
+        self.controller.snap()
+
+    def stop(self):
+        """Stop the current grab hardware wise if necessary"""
+        return ''
+
+
+if __name__ == '__main__':
+    main(__file__)
