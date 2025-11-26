2109e26dbe5e528481e98217fd3aee3796948867
Sébastien Weber <seba.weber@gmail.com>
2025-11-26 22:16:20 +0100

    with property assignement

--- packages/pymodaq_gui/src/pymodaq_gui/state/three_states.py
+++ packages/pymodaq_gui/src/pymodaq_gui/state/three_states.py
@@ -1,4 +1,6 @@
 from hatch.cli import self
+from os import environ
+environ['QT_API'] = 'pyside6'
 
 from pymodaq_gui.utils.utils import mkQApp
 from qtpy import QtWidgets, QtCore
@@ -8,6 +10,7 @@ from qtpy.QtWidgets import QWidget, QLabel, QPushButton
 from qtpy.QtStateMachine import QStateMachine, QState, QFinalState
 
 
+
 class App(QObject):
 
     def __init__(self):
@@ -15,8 +18,6 @@ class App(QObject):
 
         self.setup_ui()
         self.setup_machine()
-        self.start()
-        self.show()
 
     def setup_ui(self):
         self.widget = QWidget()
@@ -33,9 +34,13 @@ class App(QObject):
         state_2 = QState()
         state_3 = QState()
 
-        state_1.addTransition(self.push, 'clicked', state_2)
-        state_2.addTransition(self.push, 'clicked', state_3)
-        state_3.addTransition(self.push, 'clicked', state_1)
+        state_1.addTransition(self.push.clicked, state_2)
+        state_2.addTransition(self.push.clicked, state_3)
+        state_3.addTransition(self.push.clicked, state_1)
+
+        state_1.assignProperty(self.label, 'text', "I'm in State 1")
+        state_2.assignProperty(self.label, 'text', "I'm in State 2")
+        state_3.assignProperty(self.label, 'text', "I'm in State 3")
 
         self.machine.addState(state_1)
         self.machine.addState(state_2)
@@ -44,12 +49,24 @@ class App(QObject):
 
     def start(self):
         self.machine.start()
-        
+
     def show(self):
         self.widget.show()
 
 
 def main():
     app = mkQApp('StateMachine')
+
+    my_app = App()
+
+    my_app.start()
+    my_app.show()
+
+    # Run application
+    app.exec()
+
+
+if __name__ == '__main__':
+    main()
 
 
