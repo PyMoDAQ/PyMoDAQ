c41f21b078e92bc81fcf5f81164c02b5a0b89c6a
Malik Irain <malik.irain@cemes.fr>
2025-11-26 14:13:50 +0100

    better automodule

--- docs/automodule.py
+++ docs/automodule.py
@@ -1,14 +1,14 @@
-import site
-from os import system
+import sys, subprocess
+from pathlib import Path
+import importlib.util
 
-pymodaq_module_list = ["pymodaq_utils", "pymodaq_gui", "pymodaq_data"]
 
 if __name__ == "__main__":
 
-    module_path = [path for path in site.getsitepackages() if path.endswith("site-packages")][0]
-
-    for module in pymodaq_module_list:
-        system(f"sphinx-apidoc -e -t ./docs/src/_templates/apidoc -o ./docs/src/api/{module} {module_path}/{module}")
+    installed_modules = filter(lambda s : s and s.origin, map(lambda m : importlib.util.find_spec(m), ["pymodaq_utils", "pymodaq_gui", "pymodaq_data"]))
+    for module in installed_modules:
+        module_path =  Path(module.origin).parent
+        subprocess.run(["sphinx-apidoc", "-e", "-t",  "./docs/src/_templates/apidoc",  "-o", f"./docs/src/api/{module.name}", f"{module_path}"], check=True)
 
     with open('./docs/src/api/pymodaq_data/pymodaq_data.h5modules.exporter.rst', 'a') as file:
         file.write('   :exclude-members: H5Exporter\n')
