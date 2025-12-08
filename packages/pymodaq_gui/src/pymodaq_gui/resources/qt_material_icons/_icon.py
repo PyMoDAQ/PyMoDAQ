414c831f479c27485b0cd4a22962c329df637e59
Sébastien Weber <seba.weber@gmail.com>
2025-12-08 16:41:29 +0100

    get needed icon resources

--- packages/pymodaq_gui/src/pymodaq_gui/resources/qt_material_icons/_icon.py
+++ packages/pymodaq_gui/src/pymodaq_gui/resources/qt_material_icons/_icon.py
@@ -133,3 +133,7 @@ class MaterialIcon(SVGIcon):
             f'materialsymbols{style.value}/{filename}'
         )
         return path
+
+    @staticmethod
+    def resource_exists(name: str, style: Style, fill: bool, size: int) -> bool:
+        return QtCore.QFile(MaterialIcon.resource_path(name, style, fill, size)).exists()
