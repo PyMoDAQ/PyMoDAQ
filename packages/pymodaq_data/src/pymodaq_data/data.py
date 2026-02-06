d43cb6061d0949d135ce3031eeb610383c3a2e6f
Sébastien Weber <seba.weber@gmail.com>
2026-02-06 14:55:03 +0100

    add the dwa method split_as_dte to slpit a dwa in many dwa within a dte

--- packages/pymodaq_data/src/pymodaq_data/data.py
+++ packages/pymodaq_data/src/pymodaq_data/data.py
@@ -25,6 +25,7 @@ import pint
 from pint.compat import upcast_type_map
 
 from multipledispatch import dispatch
+from torch.fx.experimental.unification.multipledispatch.dispatcher import source
 
 from pymodaq_utils.enums import BaseEnum, enum_checker
 from pymodaq_utils.warnings import deprecation_msg
@@ -897,6 +898,17 @@ class DataBase(DataLowLevel, NDArrayOperatorsMixin):
     def as_dte(self, name: str = 'mydte') -> DataToExport:
         """Convenience method to wrap the DataWithAxes object into a DataToExport"""
         return DataToExport(name, data=[self])
+
+    def split_as_dte(self, name: str = 'mydte') -> DataToExport:
+        """ Convenience method to split each ndarray into a DataWithAxes object """
+        return DataToExport(name, data=[type(self)(self.labels[ind],
+                                                   source=self.source,
+                                                   dim = self.dim,
+                                                   data=[array],
+                                                   labels = [self.labels[ind]],
+                                                   axes = deepcopy(self.axes),
+                                                   units = self.units,
+                                                   ) for ind, array in enumerate(self)])
 
     def add_extra_attribute(self, **kwargs):
         for key in kwargs:
