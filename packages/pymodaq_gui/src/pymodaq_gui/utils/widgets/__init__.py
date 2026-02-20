efb574d674837de86e08cc62c2c9c8f110341c90
Ashwola <schouderconstant@outlook.com>
2026-02-20 18:18:25 +0100

    Revert "adding context_menu to list_widget"
    This reverts commit d413b46e2b194060c383cfca606358a9449c773a.

--- packages/pymodaq_gui/src/pymodaq_gui/utils/widgets/__init__.py
+++ packages/pymodaq_gui/src/pymodaq_gui/utils/widgets/__init__.py
@@ -1,5 +1,4 @@
 from .qled import QLED
-from .list_widget import ContextMenuListWidget
 from .push import PushButtonIcon, EditPush, EditPushRel
 from .spinbox import QSpinBox_ro, SpinBox, QSpinBoxWithShortcut
 from .table import TableView, TableModel
