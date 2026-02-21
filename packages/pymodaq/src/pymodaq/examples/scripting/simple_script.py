0f15845693deed812e106b101cc05fb6d1fc229e
Sébastien Weber <seba.weber@gmail.com>
2026-02-21 16:11:27 +0100

    proper saving
    could be done even better to ovoid the duplication of the nav_axes
    
    to do so should go through ModuleSaver stuff...

--- packages/pymodaq/src/pymodaq/examples/scripting/simple_script.py
+++ packages/pymodaq/src/pymodaq/examples/scripting/simple_script.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
-from pymodaq_data import Q_, DataDim
+from pymodaq_data import Q_
+from pymodaq_data.h5modules.backends import GroupType
 from pymodaq.scripting import Detector, Actuator
 
 from pymodaq_data.h5modules.data_saving import DataToExportEnlargeableSaver
 
 theta = Actuator('Angle')
 det0d = Detector('Det0D')
+det1d = Detector('Det1D')
 
 acquisitions = []
 
@@ -17,21 +19,17 @@ step = Q_('10°')
 count = (target - base)/step
 count = int(count.quantities[0].m[0]) + 1
 
-filename = Path(r'C:\Data\2026\scripted_data4.h5')
+filename = Path(r'C:\Data\2026\scripted_data1.h5')
 
 with DataToExportEnlargeableSaver(filename, enl_axis_names=('count',), enl_axis_units=('',)) as saver:
-    group = saver.add_data_group('/RawData/', DataDim.Data2D, title='scripted_data', group_name='Script')
-    value = base
 
-    for idx in range(1, count + 1):
-        acquisitions.append((value, det0d.snap().result()))
+    group = saver.add_group('Script', group_type=GroupType.data ,where='/RawData/', title='scripted_data')
+    group0d = saver.add_det_group(where=group, title=det0d.name)
+    group1d = saver.add_det_group(where=group, title=det1d.name)
+    for idx in range(count):
+        value = theta.move_abs(base + idx * step).result()  # taking directly the result makes it a blocking move
+        dte0d_future = det0d.snap()  # async snap
+        dte1d_future = det1d.snap()  # async snap
+        saver.add_data(group0d, dte0d_future.result(), (value.value(), ))
+        saver.add_data(group1d, dte1d_future.result(), (value.value(),))
 
-        value = theta.move_abs(base + idx * step)
-
-        saver.add_data(group, acquisitions[-1][1], (value.result().value(),))
-
-
-
-
-
-print(acquisitions)
