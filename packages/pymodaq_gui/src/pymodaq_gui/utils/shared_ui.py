4c80d3bf81c6537de3fd9bf0e2598f6266e40eb9
Ashwola <schouderconstant@outlook.com>
2026-04-09 19:11:38 +0200

    using stickymenu for Toolbar selection

--- packages/pymodaq_gui/src/pymodaq_gui/utils/shared_ui.py
+++ packages/pymodaq_gui/src/pymodaq_gui/utils/shared_ui.py
@@ -27,6 +27,7 @@ from pymodaq_utils.utils import get_version
 from pymodaq_utils.config import GlobalConfig as Config
 from pymodaq_utils.utils import get_module_path
 from pymodaq_gui.utils.custom_app import CustomApp
+from pymodaq_gui.utils.menu_utils import StickyMenu
 
 
 logger = set_logger(get_module_name(__file__))
@@ -183,7 +184,8 @@ class SharedUI(CustomApp):
         self.add_menu(MenuNames.VIEW, MenuNames.VIEW.capitalize(), menubar)
         self.get_menu(MenuNames.VIEW).addSeparator()
 
-        self.add_menu(MenuNames.TOOLBARS, MenuNames.TOOLBARS.capitalize(), MenuNames.VIEW)
+        self.add_menu(MenuNames.TOOLBARS, MenuNames.TOOLBARS.capitalize(), MenuNames.VIEW,
+                      menu=StickyMenu(MenuNames.TOOLBARS.capitalize()))
 
         self.add_menu(MenuNames.TOOLS, 'Tools', menubar)
         self.get_menu(MenuNames.TOOLS).addSeparator()
