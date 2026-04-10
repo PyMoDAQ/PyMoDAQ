9293c709746d5a385ed04fca4e284e641328d0a9
Malik Irain <malik.irain@cemes.fr>
2026-04-10 15:04:06 +0200

    Use a Proxy to redraw checkboxes manually + check mark icon when checked

--- packages/pymodaq_gui/src/pymodaq_gui/qt_utils.py
+++ packages/pymodaq_gui/src/pymodaq_gui/qt_utils.py
@@ -2,13 +2,19 @@
 import os
 import sys
 import warnings
+from typing import cast, Union
 
 import qt_themes
 from pyqtgraph import mkQApp as mkQApppg
 
-from qtpy import QtCore, QtWidgets, QtGui
-from qtpy.QtCore import QLocale
+from qtpy.QtWidgets import QProxyStyle, QStyle
+from qtpy.QtGui import QPainter, QColor, QPixmap
+from qtpy.QtCore import QSize
+from qtpy.QtSvg import QSvgRenderer
 
+
+from qtpy import QtCore, QtWidgets, QtGui
+from qtpy.QtCore import QLocale, Qt
 from pymodaq_utils import logger as logger_module
 from pymodaq_utils.config import GlobalConfig as Config
 
@@ -18,6 +24,36 @@ from pymodaq_gui.qvariant import QVariant
 logger = logger_module.set_logger(logger_module.get_module_name(__file__))
 
 config = Config()
+
+class WhiteCheckboxStyle(QProxyStyle):
+    def drawPrimitive(self, element:QtWidgets.QStyle.PrimitiveElement, option: QtWidgets.QStyleOption,
+                      painter: QtGui.QPainter, widget: Union[QtWidgets.QWidget, None] = None):
+        from pymodaq_gui.utils.styling import create_icon
+        if element == QStyle.PrimitiveElement.PE_IndicatorCheckBox:
+            state = option.state # type: ignore[attr-defined]
+
+            # Draw border
+            painter.save()
+            painter.setPen(QColor("white"))
+            painter.setBrush(QColor(0, 0, 0, 0))
+            if state & QStyle.StateFlag.State_MouseOver:
+                painter.setBrush(QColor(255, 255, 255, 40))
+            painter.drawRoundedRect(option.rect, 2, 2) # type: ignore[attr-defined]
+            painter.restore()
+
+            # Draw checkmark when checked
+            if state & QStyle.StateFlag.State_On:
+                size = QSize(20, 20)
+                icon = create_icon("check_small", icon_color="white")
+                pixmap = icon.pixmap(size)
+
+                icon_rect = self.baseStyle().alignedRect(
+                    Qt.LayoutDirection.LeftToRight, Qt.AlignmentFlag.AlignCenter,
+                    size, option.rect # type: ignore[attr-defined]
+                )
+                painter.drawPixmap(icon_rect.topLeft(), pixmap)
+        else:
+            super().drawPrimitive(element, option, painter, widget)
 
 
 def decode_data(encoded_data):
@@ -74,11 +110,20 @@ def center_widget_on_screen_and_show(widget: QtWidgets.QWidget):
 def start_qapplication(name='default_app') -> QtWidgets.QApplication:
     return mkQApp(name=name)
 
+def apply_styling_rules(app : QtWidgets.QApplication):
+    theme = cast(qt_themes.Theme, qt_themes.get_theme(config('gui', 'style', 'theme')[0]))
+    style = config('gui', 'style', 'style')[0]
+    qt_themes.set_theme(theme=theme, style=style)
+
+    if theme.is_dark_theme():
+        #Custom rules
+        app.setStyle(WhiteCheckboxStyle(app.style()))
+
 
 def mkQApp(name: str):
     app = mkQApppg(name)
-    qt_themes.set_theme(theme=config('gui', 'style', 'theme')[0],
-                        style=config('gui', 'style', 'style')[0])
+    apply_styling_rules(app)
+
     return app
 
 
