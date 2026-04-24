ed96fd84f9bd7bf21c3762d0d3101f5f786b3075
daarklink <axelmoyano611@gmail.com>
2026-04-24 15:45:26 +0200

    remove unused method

--- packages/pymodaq/src/pymodaq/launcher/launcher.py
+++ packages/pymodaq/src/pymodaq/launcher/launcher.py
@@ -236,15 +236,15 @@ class Launcher(CustomApp):
         self.ui_refresh()
 
 
-    def value_changed(self, param):
-        logger.debug(f'calling value_changed with param {param.name()}')
-        if param.name() == 'do_something':
-            if param.value():
-                self.settings.child('main_settings', 'something_done').setValue(True)
-            else:
-                self.settings.child('main_settings', 'something_done').setValue(False)
-
-        logger.debug(f'Value change applied')
+    # def value_changed(self, param):
+    #     logger.debug(f'calling value_changed with param {param.name()}')
+    #     if param.name() == 'do_something':
+    #         if param.value():
+    #             self.settings.child('main_settings', 'something_done').setValue(True)
+    #         else:
+    #             self.settings.child('main_settings', 'something_done').setValue(False)
+    #
+    #     logger.debug(f'Value change applied')
 
     def set_launcher_vbox(self):
         """ Set widgets in QVBox launcher section"""
