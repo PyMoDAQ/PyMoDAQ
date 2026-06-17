d8659e694ff3d9f1ea6a101a94a8c8be73362645
Sébastien Weber <seba.weber@gmail.com>
2026-06-17 23:13:48 +0200

    Update daq_move.py

--- packages/pymodaq/src/pymodaq/control_modules/daq_move.py
+++ packages/pymodaq/src/pymodaq/control_modules/daq_move.py
@@ -520,7 +520,7 @@ class DAQ_Move(ParameterControlModule):
     def _init_continuous_save(self):
         """ Initialize the continuous saving H5Saver object
 
-        Update the module_and_data_saver attribute as :class:`DetectorTimeSaver` object
+        Update the module_and_data_saver attribute as :class:`ActuatorTimeSaver` object
         """
         if self.settings.child('saver_settings', 'do_save').value():
 
