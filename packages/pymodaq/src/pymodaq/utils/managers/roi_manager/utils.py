141c33d21390f96ffe3b98c07300f87ab246119d
Sébastien Weber <seba.weber@gmail.com>
2026-06-28 11:40:30 +0200

    cleaning

--- packages/pymodaq/src/pymodaq/utils/managers/roi_manager/utils.py
+++ packages/pymodaq/src/pymodaq/utils/managers/roi_manager/utils.py
@@ -15,40 +15,6 @@ config = Config()
 logger = set_logger(get_module_name(__file__))
 
 
-
-class ModulesManager(ModulesManager):  # noqa imported from Overshooter
-    """ Customized version of the ModulesManager """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.available_data = DataToExport(self.__class__.__name__)
-
-    def get_det_data_list(self) -> DataToExport:
-        """Do a snap of selected detectors and get_actuator_value of connected actuators
-        , to get the list of all the data and processed data"""
-
-        data_det: DataToExport = super().get_det_data_list()
-
-        if len(self.actuators) == 0:
-            data_act = DataToExport(name=__class__.__name__, control_module='DAQ_Move')
-        else:
-            self.connect_actuators()
-            dte_act_to_move = DataToExport('Actuators', control_module='DAQ_MOVE')
-            for mod in self.actuators:
-                dte_act_to_move.append(mod.current_value)
-            data_act = self.move_actuators(dte_act_to_move)
-            self.connect_actuators(False)
-        data_det.append(data_act)
-
-        data_list0D = []
-        for dwa in data_det.get_data_from_dim(DataDim.Data0D):
-            data_list0D.extend([f'{dwa.origin}/{dwa.name}/{label}' for label in dwa.labels])
-
-        self.available_data = data_list0D[:]
-        return data_det
-
-
 def get_set_roi_path(subfolder: str = ''):
     """ creates and return the config folder path for rois files
     """
