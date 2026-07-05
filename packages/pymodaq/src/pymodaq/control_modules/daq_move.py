abce807a08b5ad6d5e4b24e4cd81943d4bf0f754
Sébastien Weber <seba.weber@gmail.com>
2026-07-05 13:02:21 +0200

    tests pass

--- packages/pymodaq/src/pymodaq/control_modules/daq_move.py
+++ packages/pymodaq/src/pymodaq/control_modules/daq_move.py
@@ -217,8 +217,12 @@ class DAQ_Move(ParameterControlModule):
                 config("pymodaq", "actuator", "siprefix")
                 and (unit != "" or config("pymodaq", "actuator", "siprefix_even_without_units"))
             )
+        self.update_default_abs_values()
+
+    def update_default_abs_values(self):
         self.value_changed(self.settings.child('main_settings', 'default_value_green'))
         self.value_changed(self.settings.child('main_settings', 'default_value_red'))
+
 
     @property
     def axis_names(self) -> Union[List, Dict]:
@@ -625,9 +629,7 @@ class DAQ_Move(ParameterControlModule):
         """Reload plugin settings, also updating the move_type in main_settings."""
         self.settings.child("main_settings", "move_type").setValue(self._actuator_type)
         super()._reload_plugin_settings()
-        self.value_changed(self.settings.child('main_settings', 'default_value_green'))
-        self.value_changed(self.settings.child('main_settings', 'default_value_red'))
-
+        self.update_default_abs_values()
 
     def _module_value_changed(self, param: Parameter):
         """Handle actuator-specific parameter changes."""
@@ -639,9 +641,11 @@ class DAQ_Move(ParameterControlModule):
                 self.setup_continuous_saving(param.value())
                 self.h5saver.settings.child(*path[1:]).setValue(param.value())
         elif param.name() == 'default_value_red':
-            self.ui.set_abs_value_red(Q_(param.value(), self.units))
+            if self.ui is not None:
+                self.ui.set_abs_value_red(Q_(param.value(), self.units))
         elif param.name() == 'default_value_green':
-            self.ui.set_abs_value_green(Q_(param.value(), self.units))
+            if self.ui is not None:
+                self.ui.set_abs_value_green(Q_(param.value(), self.units))
     # -------------------------------------------------------------------------
     # Thread status handler
     # -------------------------------------------------------------------------
