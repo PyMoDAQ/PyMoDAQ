8d77623e8866944a2a0aeb38bc6cbac4ed3c1386
Sébastien Weber <seba.weber@gmail.com>
2026-07-20 16:43:48 +0200

    adding creation and update of the special Subentry

--- packages/pymodaq/src/pymodaq/extensions/scan/manager/subentries.py
+++ packages/pymodaq/src/pymodaq/extensions/scan/manager/subentries.py
@@ -53,6 +53,15 @@ class SubEntryHandler(SubEntryHandler):
         """
         raise NotImplementedError
 
+    @classmethod
+    def create_subentry(cls, manager: 'ScanManager') -> ScanSubEntry:
+        pass
+
+    @classmethod
+    def update(cls, manager: 'ScanManager', entry: ScanSubEntry) -> None:
+        """ update the manager according to the loading of the SubEntry"""
+        pass
+
 
 @SubEntryHandlerFactory.register_handler()
 class ScanSettingsEntryHandler(SubEntryHandler):
@@ -87,6 +96,27 @@ class ControlModulesEntryHandler(SubEntryHandler):
         self.manager: ScanManager = None
         super().__init__(*args, **kwargs)
 
+    @classmethod
+    def create_subentry(cls, manager: 'ScanManager') -> ScanSubEntry:
+        modules_settings = Parameter.create(name='modules', type='group', children=[
+            manager.modules_manager.settings.child('detectors').saveState(),
+            manager.modules_manager.settings.child('actuators').saveState(),
+        ])
+        return ScanSubEntry(cls.handler_name,
+                            'ModuleManager',
+                            ParameterWithPath(modules_settings))
+
+    @classmethod
+    def update(cls, manager: 'ScanManager', entry: ScanSubEntry):
+        """ update the manager according to the loading of the SubEntry"""
+        module_from_daq_scan = manager.daq_scan.modules_manager
+        module_from_manager = manager.modules_manager
+        for child in entry.setting.parameter.children():
+            value = deepcopy(module_from_daq_scan.settings[child.name()])
+            value['selected'] = [mod for mod in child.value()['selected'] if
+                                 mod in value['all_items']]
+            module_from_manager.settings[child.name()] = value
+
     def execute_subentry(self, entry: ScanSubEntry,
                          manager: 'ScanManager', *args, **kwargs):
         """ Execute the given subentry
@@ -110,4 +140,62 @@ class ControlModulesEntryHandler(SubEntryHandler):
             else:
                 module_from_daq_scan.selected_detectors_name = value['selected']
             module_from_manager.settings[child.name()] = value
+
+@SubEntryHandlerFactory.register_handler()
+class ScannnerEntryHandler(SubEntryHandler):
+
+    handler_name = 'ScannerSubType'
+    use_dialog = False
+
+    def __init__(self, *args, **kwargs) -> None:
+        self.manager: ScanManager = None
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def create_subentry(cls, manager: 'ScanManager') -> ScanSubEntry:
+        scanner = manager.scanner
+        scan_settings = Parameter.create(name='type_subtype', type='group', children=[
+            scanner.settings.child('scan_type').saveState(),
+            scanner.settings.child('scan_sub_type').saveState(),
+            scanner.scanner.settings.saveState(),
+        ])
+        return ScanSubEntry(cls.handler_name,
+                            'Scanner',
+                            ParameterWithPath(scan_settings))
+
+    @classmethod
+    def update(cls, manager: 'ScanManager', entry: ScanSubEntry):
+        """ update the manager according to the loading of the SubEntry"""
+        pass
+
+    def execute_subentry(self, entry: ScanSubEntry,
+                         manager: 'ScanManager', *args, **kwargs):
+        """ Execute the given subentry
+
+        In general, should get first the module on which the settings will be applied
+        Then apply the Settings subentry to this module
+
+        Examples
+        --------
+        module = self.get_module(entry, *args, **kwargs)
+        module.settings.child(*entry.setting.path).setValue(entry.setting.value())
+        """
+        scanner_from_daq_scan = self.manager.daq_scan.scanner
+        scanner_from_manager = self.manager.scanner
+
+        scan_type = entry.setting.parameter['scan_type']
+        scan_sub_type = entry.setting.parameter['scan_sub_type']
+
+        scanner_from_manager.settings['scan_type'] = scan_type
+        scanner_from_daq_scan.settings['scan_type'] = scan_type
+        QtWidgets.QApplication.processEvents()
+        scanner_from_manager.settings['scan_sub_type'] = scan_sub_type
+        scanner_from_daq_scan.settings['scan_sub_type'] = scan_sub_type
+        QtWidgets.QApplication.processEvents()
+
+        scanner_from_manager.scanner.settings.restoreState(
+            entry.setting.parameter.child('scanner_settings').saveState())
+        scanner_from_daq_scan.scanner.settings.restoreState(
+            entry.setting.parameter.child('scanner_settings').saveState())
+
 
