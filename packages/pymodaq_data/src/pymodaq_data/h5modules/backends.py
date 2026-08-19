487e37bec28014c899a4f9d2eeb9969962c158a0
Sébastien Weber <seba.weber@gmail.com>
2026-08-19 21:17:21 +0200

    typing

--- packages/pymodaq_data/src/pymodaq_data/h5modules/backends.py
+++ packages/pymodaq_data/src/pymodaq_data/h5modules/backends.py
@@ -875,7 +875,7 @@ class H5Backend:
 
         return name.lower() in [name.lower() for name in self.get_children(where)]
 
-    def get_node(self, where, name=None) -> Node:
+    def get_node(self, where, name=None) -> Node | GROUP:
         if isinstance(where, Node):
             where = where.node
         try:
