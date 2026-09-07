44c205c39027be53d7e7fc7bee1176482cde9f61
Sébastien Weber <seba.weber@gmail.com>
2026-09-07 11:15:03 +0200

    restoring disapeared module_and_data_saver
    due to copy/move of custom ext (was duplicated in extenstions and in utils....)

--- packages/pymodaq/src/pymodaq/utils/custom_ext.py
+++ packages/pymodaq/src/pymodaq/utils/custom_ext.py
@@ -8,6 +8,7 @@ from pymodaq_utils.enums import StrEnum
 from pymodaq_gui.utils import CustomApp, DockArea
 
 from pymodaq.utils.managers.modules.modules_manager import ModulesManager
+from pymodaq.utils.h5modules.module_saving import ModuleSaver
 
 if TYPE_CHECKING:
     from pymodaq.dashboard import DashBoard
@@ -32,7 +33,6 @@ class CustomExt(CustomApp):
 
         self.dashboard = dashboard
 
-        self.runner_thread : QtCore.QThread = None
         if dashboard is not None:
             self._modules_manager = module_manager_class(
                 detectors=self.dashboard.detector_modules,
@@ -57,8 +57,7 @@ class CustomExt(CustomApp):
         """Method to be subclassed in order to define a custom quit function
         """
         super().quit_fun()
-        if self.runner_thread is not None:
-            self.exit_runner_thread()
+
         if self.dashboard is not None:
             self.show_dashboard(True)  #make sure to show it if it was hidden
 
@@ -112,12 +111,6 @@ class CustomExt(CustomApp):
         """
         return self._modules_manager
 
-    def exit_runner_thread(self, duration : int = 5000):
-        self.runner_thread.quit()
-        terminated = self.runner_thread.wait(duration)
-        if not terminated:
-            self.runner_thread.terminate()
-            self.runner_thread.wait()
 
     def create_dashboard_toolbar(self,
                                  add_dashboard: bool = True,
@@ -156,3 +149,17 @@ class CustomExt(CustomApp):
             self.dashboard.mainwindow.setWindowTitle('Dashboard')
             self.dashboard.mainwindow.closeEvent = lambda event: self.set_action_checked(DashBoardToolbarActions.SHOW,
                                                                                          False)
+
+    @property
+    def module_and_data_saver(self):
+        """ Complex but Standardized way to save data from the CustomExt and
+        especially from the ControlModules"""
+        if (self._module_and_data_saver.h5saver is None
+                or not self._module_and_data_saver.h5saver.isopen()):
+            self._module_and_data_saver.h5saver = self.h5saver
+        return self._module_and_data_saver
+
+    @module_and_data_saver.setter
+    def module_and_data_saver(self, mod: ModuleSaver):
+        self._module_and_data_saver = mod
+        self._module_and_data_saver.h5saver = self.h5saver
