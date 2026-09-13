20523cc11ecbecd1c857cd4e5de6cd8e080fdf7e
Sébastien Weber <seba.weber@gmail.com>
2026-09-13 10:44:26 +0200

    creating and using a categorizing module in pymodaq_utils
    used by ModuleSelector, Experiment Manager and Sequencer Move elt

--- packages/pymodaq/src/pymodaq/control_modules/control_module_selector.py
+++ packages/pymodaq/src/pymodaq/control_modules/control_module_selector.py
@@ -3,9 +3,7 @@ from qtpy import QtWidgets, QtCore
 from pymodaq.control_modules.instruments import DET_TYPES
 from pymodaq_gui.utils.menu_utils import build_menu_from_iterable
 
-REMOTE_ITEMS = {'LECODirector', 'TCPServer'}
-MOCK_ITEMS = {}
-
+from pymodaq_utils.categorizing import categorize_items, find_last_index, add_category_layers, REMOTE_ITEMS, MOCK_ITEMS
 
 
 class ModuleSelector(QtCore.QObject):
@@ -44,51 +42,6 @@ class ModuleSelector(QtCore.QObject):
         self.add_widget.adjustSize()
         self.module_changed.emit(path_tuple)
 
-
-def categorize_items(item_list, remote_items=None, mock_items=None):
-    """
-    Core function: categorize any list of items into Mock/Plugin/Remote.
-
-    Args:
-        item_list: List of items to categorize
-        remote_items: Custom set of remote items (optional)
-        mock_items: Custom set of mock items (optional)
-
-    Returns: dict {category: [items]} with only non-empty categories
-    """
-    remote_items = remote_items or REMOTE_ITEMS
-    mock_items = mock_items or MOCK_ITEMS
-
-    categorized = {'Remote': [], 'Mock': [], 'Plugin': []}
-
-    for item in item_list:
-        if item in remote_items:
-            categorized['Remote'].append(item)
-        elif item in mock_items or 'mock' in item.lower():
-            categorized['Mock'].append(item)
-        else:
-            categorized['Plugin'].append(item)
-
-    # Return only non-empty categories
-    return {k: v for k, v in categorized.items() if v}
-
-def add_category_layers(dimension_dict, remote_items=None, mock_items=None):
-    """
-    Add category layers to a dimension dictionary.
-    Uses categorize_items for each dimension.
-
-    Args:
-        dimension_dict: {dimension: [items]}
-
-    Returns: {dimension: {category: [items]}}
-    """
-    result = {}
-
-    for dimension, items in dimension_dict.items():
-        # Reuse the core categorization function
-        result[dimension] = categorize_items(items, remote_items, mock_items)
-
-    return result
 
 
 
