e4d82c86bf89982afdb35bcb04207e7f0e84e29c
Sébastien Weber <seba.weber@gmail.com>
2026-09-17 20:47:11 +0200

    specify opening file mode in h5manager + patch for no forcing opening if file already openede

---
+++ packages/pymodaq_gui/src/pymodaq_gui/parameter/enums.py
@@ -0,0 +1,14 @@
+from pymodaq_utils.enums import StrEnum
+
+
+class ParameterChangeType(StrEnum):
+    """Enumeration of native change types emitted by PyQtGraph Parameters."""
+    VALUE = "value"
+    LIMITS = "limits"
+    OPTIONS = "options"
+    CHILD_ADDED = "childAdded"
+    PARENT = "parent"
+    NAME = "name"
+    READ_ONLY = "readOnly"
+    VISIBLE = "visible"
+    CONTEXT_MENU = "contextMenu"
